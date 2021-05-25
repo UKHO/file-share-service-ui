eee6d5b445d5001b9465fc1c9cbe189081b5037f
Ravi102465 <Ravi102465@mastek.com>
2021-05-25 19:24:54 +0530

    AB#10145 Initial changes

---
+++ Deployment/src/Modules/Storage/outputs.tf
@@ -0,0 +1,11 @@
+output name {
+  value = azurerm_storage_account.storage.name
+}
+
+output storage_account_id {
+  value = azurerm_storage_account.storage.id
+}
+
+output web_endpoint {
+  value = azurerm_storage_account.storage.primary_web_endpoint
+}
---
+++ Deployment/src/outputs.tf
@@ -0,0 +1,7 @@
+output "website_url" {
+   value = module.storage.web_endpoint
+}
+
+output "website_storage_account_name" {
+   value = module.storage.name
+}
