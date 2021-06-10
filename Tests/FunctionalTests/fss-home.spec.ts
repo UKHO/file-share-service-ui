3a9b3c32133ed1848906d64fae4770a2032a9518
Rashi Mishra <Rashi14748@Mastek.Com>
2021-06-10 21:28:46 +0530

    10257-create functional and accessibility tests

--- Tests/FunctionalTests/fss-home.spec.ts
+++ Tests/FunctionalTests/fss-home.spec.ts
@@ -1,19 +1,13 @@
-// Needs to be higher than the default Playwright timeout
-jest.setTimeout(40 * 1000)
 import { Browser, BrowserContext, chromium, Page } from 'playwright';
 const { autoTestConfig } = require('./appSetting');
 
-//declare const page: Page;
 describe('Launch Browser', () => {
 
     let browser: Browser;
     let context: BrowserContext;
     let page: Page;
     beforeAll(async () => {
-        browser = await chromium.launch({
-            channel: "chrome",
-            headless: false
-        })
+        browser = await chromium.launch();
         context = await browser.newContext();
         page = await context.newPage();
         await page.goto(autoTestConfig.url)
@@ -24,11 +18,6 @@ describe('Launch Browser', () => {
         await context.close()
         await browser.close()
     })
-
-    // it("should navigate to admirality once you click on 'admirality'", async () => {
-    //     await page.click("#a");
-    //     expect(page.url()).toMatch('https://www.admiralty.co.uk/cookie-policy')
-    // })
 
     test('Does it contains search with text', async () => {
         await page.textContent('"Search"');
