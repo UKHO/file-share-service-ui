c9fde5a875de8f00174f28192393415528d1917c
Rashi Mishra <Rashi14748@Mastek.Com>
2021-06-10 21:56:38 +0530

    10257-added functional tests

--- Tests/FunctionalTests/fss-home.spec.ts
+++ Tests/FunctionalTests/fss-home.spec.ts
@@ -7,9 +7,15 @@ describe('Launch Browser', () => {
     let context: BrowserContext;
     let page: Page;
     beforeAll(async () => {
-        browser = await chromium.launch();
+        browser = await chromium.launch({
+            headless: false,
+            channel: "chrome"
+        });
         context = await browser.newContext();
         page = await context.newPage();
+    })
+
+    beforeEach(async () => {
         await page.goto(autoTestConfig.url)
     })
 
@@ -21,6 +27,14 @@ describe('Launch Browser', () => {
 
     test('Does it contains search with text', async () => {
         await page.textContent('"Search"');
+    })
+
+    test('Aclick', async () => {
+        await page.click('text="Accessibility"')
+    })
+
+    test('PPclick', async () => {
+        await page.click('text="Privacy policy"')
     })
 
 })
