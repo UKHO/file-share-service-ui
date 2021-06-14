f6f3ca5eec0f3981f6eb244bdf0ed39f2934f222
Chaitali Shidid <chaital14503@mastek.com>
2021-06-14 11:33:00 +0530

    10197-Footer component-updated ukho-design package and target=blank-AB#10197 AB#1022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/app.shared.module';
-
-import { environment } from 'src/environments/environment';
-
 @NgModule({
   declarations: [
     AppComponent
