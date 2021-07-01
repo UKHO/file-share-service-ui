9649b07d5fc9c80c22728116ba6b81c5660ead7c
Shrikant Gautam <Shrikan14443@Mastek.Com>
2021-07-01 16:03:45 +0530

    10266-enabled hash routing AB#10266 AB#10268

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -30,7 +30,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,{
-    useHash: false,
+    useHash: true,
     initialNavigation: 'enabled'
   })],
   exports: [RouterModule]
