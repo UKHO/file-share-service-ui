1e3f18385c86711a0c31412cbadd3db0b040aef0
Shreyan13680 <Shreyan13680@IN-MH1DSW101226>
2021-07-08 19:32:36 +0530

    10203 Changes to resolve failing unit Tests AB#10203 AB#10328

--- Tests/UnitTests/fss-search-filter.service.spec.ts
+++ Tests/UnitTests/fss-search-filter.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { FssSearchRow, IFssSearchService} from '../../src/app/core/models/fss-search-types';
+import { Field, Operator, FssSearchRow, IFssSearchService } from '../../src/app/core/models/fss-search-types';
 
 import { FssSearchFilterService } from '../../src/app/core/services/fss-search-filter.service';
 import { FssSearchService } from '../../src/app/core/services/fss-search.service';
@@ -7,21 +7,25 @@ import { FssSearchService } from '../../src/app/core/services/fss-search.service
 describe('FssSearchFilterService', () => {
   let service: FssSearchFilterService;
   let searchService: IFssSearchService;
-  
+
   beforeEach(() => {
     TestBed.configureTestingModule({});
     searchService = TestBed.inject(FssSearchService);
-    service = TestBed.inject(FssSearchFilterService); 
+    service = TestBed.inject(FssSearchFilterService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
- 
+
   // Test for single searcg criteria
   test('should create valid filter expression for single search criteria', () => {
     let searchRows: FssSearchRow[] = [];
-    searchRows.push(createSearchRow(1, 'AND', 'FileName', 'eq', 'Test.txt'));
+    let fields: Field[] = [];
+    let operators: Operator[] = [];
+    fields.push({ value: 'FileName', text: '@FileName', type: 'SystemAttribute', dataType: 'string' });
+    operators.push({ value: 'eq', text: '=', type: 'operator', supportedDataTypes: ['string', 'number', 'date', 'attribute'] });
+    searchRows.push(createSearchRow(1, fields, operators, 'AND', 'FileName', 'eq', 'Test.txt', 'text', false));
     var filter = service.getFilterExpression(searchRows);
 
     expect(filter).toBe("FileName eq 'Test.txt'");
@@ -30,23 +34,37 @@ describe('FssSearchFilterService', () => {
   //Test for multiple search criteria
   test('should create valid filter expression for multiple search criteria', () => {
     let searchRows: FssSearchRow[] = [];
-    searchRows.push(createSearchRow(1, 'AND', 'FileName', 'eq', 'TestReport.pdf'));
-    searchRows.push(createSearchRow(2, 'OR', 'FileSize',  'le', 3000));    
-    searchRows.push(createSearchRow(3, 'AND', 'ExpiryDate', 'gt', '2021-12-31T13:00:00'));
+    let fields: Field[] = [
+      { value: 'FileName', text: '@FileName', type: 'SystemAttribute', dataType: 'string' },
+      { value: 'FileSize', text: '@FileSize', type: 'SystemAttribute', dataType: 'number' },
+      { value: 'ExpiryDate', text: '@BatchExpiryDate', type: 'SystemAttribute', dataType: 'date' },
+    ];
+    let operators: Operator[] = [
+      { value: 'eq', text: '=', type: 'operator', supportedDataTypes: ['string', 'number', 'date', 'attribute'] },
+      { value: 'gt', text: '>', type: 'operator', supportedDataTypes: ['number', 'date'] },
+      { value: 'le', text: '<=', type: 'operator', supportedDataTypes: ['number', 'date'] }
+    ];
+    searchRows.push(createSearchRow(1, fields,operators,'AND', 'FileName', 'eq', 'TestReport.pdf', 'text', false));
+    searchRows.push(createSearchRow(2, fields,operators,'OR', 'FileSize', 'le', 3000, 'tel', false));
+    searchRows.push(createSearchRow(3,fields,operators, 'AND', 'ExpiryDate', 'gt', '2021-12-31T13:00:00.000Z', 'date', false));
 
     var filter = service.getFilterExpression(searchRows);
 
-    expect(filter).toBe("FileName eq 'TestReport.pdf' OR FileSize le 3000 AND ExpiryDate gt 2021-12-31T13:00:00");
+    expect(filter).toBe("FileName eq 'TestReport.pdf' OR FileSize le 3000 AND ExpiryDate gt 2021-12-31T13:00:00.000Z");
   });
 });
 
-export function createSearchRow(rowId: number, joinOperator: string, field: string, operator: string, value: any) {
+export function createSearchRow(rowId: number, fields: Field[], operators: Operator[], joinOperator: string, field: string, operator: string, value: any, valueType: "time" | "text" | "date" | "email" | "password" | "tel" | "url", valueIsdisabled: boolean) {
   var row = new FssSearchRow();
   row.rowId = rowId;
+  row.fields = fields,
+    row.operators = operators;
   row.selectedJoinOperator = joinOperator;
   row.selectedField = field;
   row.selectedOperator = operator;
   row.value = value;
+  row.valueType = valueType;
+  row.valueIsdisabled = valueIsdisabled;
   return row;
 }
 
