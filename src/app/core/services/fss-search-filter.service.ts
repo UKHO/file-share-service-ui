46b25de70b2f16974c4268446a3886b51f2cc5b6
Shrikant Gautam <Shrikan14443@Mastek.Com>
2021-07-15 16:12:07 +0530

    10133 - removed commented code AB#10133 AB#10371

--- src/app/core/services/fss-search-filter.service.ts
+++ src/app/core/services/fss-search-filter.service.ts
@@ -71,17 +71,4 @@ export class FssSearchFilterService {
     return operatorType
   }
 
-  // getAsDate(day:any, time:any) {
-  //   var hours = Number(time.match(/^(\d+)/)[1]);
-  //   var minutes = Number(time.match(/:(\d+)/)[1]);
-  //   var sHours = hours.toString();
-  //   var sMinutes = minutes.toString();
-  //   if (hours < 10) sHours = "0" + sHours;
-  //   if (minutes < 10) sMinutes = "0" + sMinutes;
-  //   time = sHours + ":" + sMinutes + ":00";
-  //   var d = new Date(day);
-  //   var n = d.toISOString().substring(0, 10);
-  //   var newDate = new Date(n + "T" + time);
-  //   return newDate;
-  // }
 }
