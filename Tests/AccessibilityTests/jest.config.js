83604dc1277429f8fdcc7247b41edaa818d88158
Amogh Lanke <amogh.lanke@mastek.conm>
2021-07-22 11:36:38 +0530

    AB#10575 jest config change to publish  test results in junit.xml for Accessibility and Functional Tests

--- Tests/AccessibilityTests/jest.config.js
+++ Tests/AccessibilityTests/jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   preset: 'jest-preset-angular',
   roots: ['<rootDir>/'],
   testMatch: ['**/+(*.)+(spec).+(ts)'],
+  reporters: ["default","jest-junit"],
   setupFilesAfterEnv:  ['<rootDir>/setup-jest.ts'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
     prefix: '<rootDir>/'
