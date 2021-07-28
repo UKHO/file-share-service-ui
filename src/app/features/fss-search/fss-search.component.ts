75130631e55bbdca022a96ed3c5e5a938117a935
Kajal Kundu <kajalk@mastek.com>
2021-07-28 10:37:02 +0100

    10372-merge main resolve conflicts AB#10372

--- src/app/features/fss-search/fss-search.component.ts
+++ src/app/features/fss-search/fss-search.component.ts
@@ -5,6 +5,7 @@ import { Operator, IFssSearchService, Field, JoinOperator, FssSearchRow } from '
 import { FileShareApiService } from '../../core/services/file-share-api.service';
 import { FssSearchFilterService } from '../../core/services/fss-search-filter.service';
 
+
 @Component({
   selector: 'app-fss-search',
   templateUrl: './fss-search.component.html',
@@ -30,6 +31,12 @@ export class FssSearchComponent implements OnInit {
   userAttributes: Field[] = [];
   errorMessageTitle: string = "";
   errorMessageDescription: string = "";
+  pageRecordCount: number = 10;
+  searchResultTotal: number;
+  pagingLinks: any = [];
+  pages: number;
+  currentPage: number = 0;
+  paginatorLabel: string;
   constructor(private fssSearchTypeService: IFssSearchService, private fssSearchFilterService: FssSearchFilterService, private fileShareApiService: FileShareApiService) { }
 
   ngOnInit(): void {
@@ -151,7 +158,7 @@ export class FssSearchComponent implements OnInit {
         var isNumber = reg.test(this.fssSearchRows[rowId].value);
         if (!isNumber) {
           this.errorMessageTitle = "Please provide only Numbers against FileSize";
-          this.errorMessageDescription = "Incorrect value '" + this.fssSearchRows[rowId].value + "' on row " + (rowId+1);
+          this.errorMessageDescription = "Incorrect value '" + this.fssSearchRows[rowId].value + "' on row " + (rowId + 1);
           flag = false;
           break;
         }
@@ -167,8 +174,28 @@ export class FssSearchComponent implements OnInit {
       console.log(filter);
       if (filter != null) {
         this.searchResult = [];
-        this.fileShareApiService.getSearchResult(filter).subscribe((res) => {
-          this.handleSuccess(res)
+        this.fileShareApiService.getSearchResult(filter, false).subscribe((res) => {
+          this.searchResult = res;
+          if (this.searchResult.count > 0) {
+            var searchResultCount = this.searchResult['count'];
+            this.searchResultTotal = this.searchResult['total'];
+            this.currentPage = 1;
+            this.pages = this.searchResultTotal % searchResultCount === 0 ?
+              Math.floor(this.searchResultTotal / searchResultCount) :
+              (Math.floor(this.searchResultTotal / searchResultCount) + 1);
+            this.handleSuccess()
+          }
+          else {
+            this.searchResult = [];
+            this.displaySearchResult = false;
+            this.showMessage(
+              "info",
+              "No results can be found for this search",
+              "Try again using different parameters in the search query."
+            );
+            this.displayLoader = false;
+          }
+
         },
           (error) => {
             this.handleErrMessage(error);
@@ -198,36 +225,57 @@ export class FssSearchComponent implements OnInit {
     this.displayMessage = true;
   }
 
-  handleSuccess(res: any) {
-    if (res.count > 0) {
-      this.searchResult = res;
-      this.searchResult = Array.of(this.searchResult['entries']);
-      this.displaySearchResult = true;
-      this.hideMessage();
-      this.displayLoader = false;
-    }
-    else{
-      this.searchResult = [];
-      this.displaySearchResult = false;
-      this.showMessage(
-        "info",
-        "No results can be found for this search",
-        "Try again using different parameters in the search query."
-      );
-      this.displayLoader = false;
-    }
-  }
+  handleSuccess() {
 
-  handleErrMessage(err: any){
+    this.pagingLinks = this.searchResult['_Links'];
+    this.searchResult = Array.of(this.searchResult['entries']);
+    this.displaySearchResult = true;
+    this.hideMessage();
+    this.setPaginatorLabel(this.currentPage);
     this.displayLoader = false;
-    var errmsg="";
-    if(err.error != undefined && err.error.total>0){    
-        for(let i=0; i<err.error.errors.length; i++){
-            errmsg += err.error.errors[i]['description']+'\n';
-        }
-        this.showMessage("warning","An exception occurred when processing this search",errmsg);
-    }   
   }
 
+  private setPaginatorLabel(currentPage: number) {
+    this.paginatorLabel = "Showing " + (((currentPage * this.pageRecordCount) - this.pageRecordCount) + 1) +
+      "-" + (((currentPage * this.pageRecordCount) > this.searchResultTotal) ? this.searchResultTotal : (currentPage * this.pageRecordCount)) + " of " + this.searchResultTotal;
+  }
 
+  handleErrMessage(err: any) {
+    this.displayLoader = false;
+    var errmsg = "";
+    if (err.error != undefined && err.error.total > 0) {
+      for (let i = 0; i < err.error.errors.length; i++) {
+        errmsg += err.error.errors[i]['description'] + '\n';
+      }
+      this.showMessage("warning", "An exception occurred when processing this search", errmsg);
+    }
+  }
+  pageChange(currentPage: number) {
+    this.displayLoader = true;
+    var paginatorAction = this.currentPage > currentPage ? "prev" : "next";
+    this.currentPage = currentPage;
+    if (paginatorAction === "next") {
+      var nextPageLink = this.pagingLinks!.next!.href;
+        this.fileShareApiService.getSearchResult(nextPageLink, true).subscribe((res) => {
+        this.searchResult = res;
+        this.handleSuccess()
+      },
+        (error) => {
+          this.handleErrMessage(error);
+        }
+      );
+    }
+    else if (paginatorAction === "prev") {
+      console.log(this.pagingLinks!);
+      var previousPageLink = this.pagingLinks!.previous!.href;
+      this.fileShareApiService.getSearchResult(previousPageLink, true).subscribe((res) => {
+        this.searchResult = res;
+        this.handleSuccess()
+      },
+        (error) => {
+          this.handleErrMessage(error);
+        }
+      );
+    }
+  }
 }
