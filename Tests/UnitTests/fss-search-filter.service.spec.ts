98c1ca8acf751c9f511dbff51b9a42f19d668cd8
Shreyanka Dhamankar <Shreyan13680@Mastek.Com>
2021-07-29 18:13:42 +0530

    Code changes to add unit test cases for query part of grouping AB#10709 AB#10716

--- Tests/UnitTests/fss-search-filter.service.spec.ts
+++ Tests/UnitTests/fss-search-filter.service.spec.ts
@@ -50,10 +50,43 @@ describe('FssSearchFilterService', () => {
     searchRows.push(createSearchRow(2, fields, operators, 'OR', 'FileSize', 'le', 3000, 'tel', false));
     searchRows.push(createSearchRow(3, fields, operators, 'AND', 'ExpiryDate', 'gt', '2021-12-31T13:00:00.000Z', 'date', false));
     var filter = service.getFilterExpression(searchRows, groupings);
-    groupings.push({startIndex: 1,endIndex:3});
     expect(filter).toBe("FileName eq 'TestReport.pdf' OR FileSize le 3000 AND ExpiryDate gt 2021-12-31T13:00:00.000Z");
   });
+
+  //Test for multiple search criteria with qury grouping
+  test('should create valid filter expression for multiple search criteria for grouping query', () => {
+    let searchRows: FssSearchRow[] = [];
+    let groupings: RowGrouping[] = [];
+    let fields: Field[] = [
+      { value: 'FileName', text: '@FileName', type: 'SystemAttribute', dataType: 'string' },
+      { value: 'FileSize', text: '@FileSize', type: 'SystemAttribute', dataType: 'number' },
+      { value: 'FileSize', text: '@FileSize', type: 'SystemAttribute', dataType: 'number' },
+      { value: 'FileSize', text: '@FileSize', type: 'SystemAttribute', dataType: 'number' },
+      { value: 'FileSize', text: '@FileSize', type: 'SystemAttribute', dataType: 'number' },
+    ];
+    let operators: Operator[] = [
+      { value: 'eq', text: '=', type: 'operator', supportedDataTypes: ['string', 'number', 'date', 'attribute'] },
+      { value: 'gt', text: '>', type: 'operator', supportedDataTypes: ['number', 'date'] },
+      { value: 'lt', text: '<', type: 'operator', supportedDataTypes: ['number', 'date'] },
+      { value: 'gt', text: '<', type: 'operator', supportedDataTypes: ['number', 'date'] },
+      { value: 'gt', text: '<', type: 'operator', supportedDataTypes: ['number', 'date'] }
+    ];
+    searchRows.push(createSearchRow(1, fields, operators,'AND','FileName', 'eq', 'TestReport.pdf', 'text', false));
+    searchRows.push(createSearchRow(2, fields, operators, 'AND', 'FileSize', 'gt', 10, 'tel', false));
+    searchRows.push(createSearchRow(3, fields, operators, 'OR', 'FileSize', 'lt', 10000000,'tel', false));
+    searchRows.push(createSearchRow(4, fields, operators, 'AND', 'FileSize', 'gt', 100000000,'tel', false));
+    searchRows.push(createSearchRow(5, fields, operators, 'AND', 'FileSize', 'gt', 1000000000,'tel', false));
+    groupings.push({startIndex: 0,endIndex:4});
+    groupings.push({startIndex: 1,endIndex:4});
+    groupings.push({startIndex: 1,endIndex:2});
+    groupings.push({startIndex: 2,endIndex:3});
+    var filter = service.getFilterExpression(searchRows, groupings);
+    
+    expect(filter).toBe("(FileName eq 'TestReport.pdf' AND ((FileSize gt 10 OR (FileSize lt 10000000) AND FileSize gt 100000000) AND FileSize gt 1000000000))");
+  });
 });
+
+
 
 export function createSearchRow(rowId: number, fields: Field[], operators: Operator[], joinOperator: string, field: string, operator: string, value: any, valueType: "time" | "text" | "date" | "email" | "password" | "tel" | "url", valueIsdisabled: boolean) {
   var row = new FssSearchRow();
