0fb00e2e0a05655ec5fc2c6930e7867b51bad7bd
Chaitali Shidid <chaital14503@mastek.com>
2021-08-04 14:41:11 +0530

    10722-search-unit test cases updated-AB#10722 AB#10769

--- Tests/UnitTests/fss-search.component.spec.ts
+++ Tests/UnitTests/fss-search.component.spec.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { TestBed } from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA , ViewChild, ElementRef } from '@angular/core';
+import { NO_ERRORS_SCHEMA, ViewChild, ElementRef } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ButtonModule, SelectModule, CheckboxModule, TextinputModule, DialogueModule, ExpansionModule, CardModule, TableModule } from '@ukho/design-system';
+import { MsalService, MSAL_INSTANCE } from '@azure/msal-angular';
 
 import { FilterPipe } from '../../src/app/features/fss-search/filter.pipe';
 import { FssSearchResultsComponent } from '../../src/app/features/fss-search/fss-search-results/fss-search-results.component';
@@ -14,6 +15,7 @@ import { AppConfigService } from '../../src/app/core/services/app-config.service
 import { FssSearchService } from '../../src/app/core/services/fss-search.service';
 import { FssSearchFilterService } from '../../src/app/core/services/fss-search-filter.service';
 import { Field, FssSearchRow, Operator } from '../../src/app/core/models/fss-search-types';
+import { PublicClientApplication } from '@azure/msal-browser';
 
 describe('FssSearchComponent', () => {
   let component: FssSearchComponent;
@@ -22,6 +24,7 @@ describe('FssSearchComponent', () => {
   let searchFilterservice: FssSearchFilterService;
   let searchService: FssSearchService;
   let elementRef: ElementRef;
+  let msalService: MsalService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -32,7 +35,12 @@ describe('FssSearchComponent', () => {
         FssSearchRowComponent,
         FssSearchResultsComponent,
         FilterPipe],
-      providers: [FileShareApiService],
+        providers: [
+          {
+            provide: MSAL_INSTANCE,
+            useFactory: MockMSALInstanceFactory       
+          },
+           MsalService],
       schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
@@ -44,6 +52,7 @@ describe('FssSearchComponent', () => {
     searchService = TestBed.inject(FssSearchService);
     searchFilterservice = TestBed.inject(FssSearchFilterService);
     fileShareApiService = TestBed.inject(FileShareApiService);
+    msalService =TestBed.inject(MsalService);
   });
 
   it('should create', () => {
@@ -53,7 +62,7 @@ describe('FssSearchComponent', () => {
   });
 
   test('should return datatype as "attribute" when UserAttribute is passed', () => {
-    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef);
+    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef, msalService);
     component.ngOnInit();
     component.fields = searchService.getFields(MockUserAttributeFields());
     console.log(component);
@@ -61,7 +70,7 @@ describe('FssSearchComponent', () => {
   });
 
   test('should return datatype as "string" when SystemAttribute FileName is passed', () => {
-    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef);
+    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef, msalService);
     component.ngOnInit();
     component.fields = searchService.getFields(MockUserAttributeFields());
     console.log(component);
@@ -69,7 +78,7 @@ describe('FssSearchComponent', () => {
   });
 
   test('should return filtered operators based on number datatype', () => {
-    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef);
+    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef, msalService);
     component.ngOnInit();
     const expectedOperators: Operator[] = [
       { value: 'eq', text: '=', type: 'operator', supportedDataTypes: ['string', 'number', 'date', 'attribute'] },
@@ -84,7 +93,7 @@ describe('FssSearchComponent', () => {
   });
 
   test('should return value type text based on datatype string', () => {
-    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef);
+    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef, msalService);
     component.ngOnInit();
     var expectedValueType = "text";
     var result = component.getValueType('string');
@@ -93,7 +102,7 @@ describe('FssSearchComponent', () => {
   });
 
   test('should return operator type nullOperator based on operator value ne null', () => {
-    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef);
+    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef, msalService);
     component.ngOnInit();
     var expectedOperatorType = "nullOperator";
     var changedOperator = { operatorValue: "ne null", rowId: 1 }
@@ -103,7 +112,7 @@ describe('FssSearchComponent', () => {
   });
 
   test('should return second row based on rowid passed', () => {
-    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef);
+    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef, msalService);
     component.ngOnInit();
     let searchRows: FssSearchRow[] = [];
     let fields: Field[] = [
@@ -157,7 +166,7 @@ describe('FssSearchComponent', () => {
   });
 
   test('should return flag false when FileSize value is passed as number', () => {
-    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef);
+    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef, msalService);
     component.ngOnInit();
     let searchRows: FssSearchRow[] = [];
     let fields: Field[] = [
@@ -180,7 +189,7 @@ describe('FssSearchComponent', () => {
   });
 
   test('should return row count 2 when Add new line is called twice', () => {
-    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef);
+    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef, msalService);
     component.ngOnInit();
     component.fields = searchService.getFields(MockUserAttributeFields());
     component.addSearchRow();
@@ -191,7 +200,7 @@ describe('FssSearchComponent', () => {
   });
 
   test('should return row count 2 when 3 rows exist and delete is called', () => {
-    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef);
+    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef, msalService);
     component.ngOnInit();
     component.fields = searchService.getFields(MockUserAttributeFields());
     component.addSearchRow();
@@ -204,9 +213,9 @@ describe('FssSearchComponent', () => {
   });
 
   test('should disable Value input when nullOperator is selected in the row', () => {
-    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef);
+    component = new FssSearchComponent(searchService, searchFilterservice, fileShareApiService, elementRef, msalService);
     component.ngOnInit();
-    component.fields = searchService.getFields(MockUserAttributeFields());    
+    component.fields = searchService.getFields(MockUserAttributeFields());
     component.fssSearchRows.push(createSearchRow(1, component.fields, component.operators, 'AND', 'FileSize', 'nullOperator', 'test', 'tel', false));
     component.toggleValueInput(component.fssSearchRows[0], 'nullOperator');
     var result = component.fssSearchRows[0].valueIsdisabled;
@@ -243,3 +252,19 @@ export function createSearchRow(rowId: number, fields: Field[], operators: Opera
   row.valueIsdisabled = valueIsdisabled;
   return row;
 }
+export function MockMSALInstanceFactory () {    
+  return new PublicClientApplication ( {
+     auth:{
+       clientId:"",
+       authority: "",
+       redirectUri: "/search",
+       knownAuthorities: [],
+       postLogoutRedirectUri: "/",
+       navigateToLoginRequestUrl: false
+     },
+     cache:{
+       cacheLocation: "localStorage",
+       storeAuthStateInCookie: true
+     }
+   })           
+ };
