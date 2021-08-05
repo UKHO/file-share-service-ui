eb0d771709cd0572689ff2d944fa487b371f7f2b
Amogh Lanke <amogh.lanke@mastek.conm>
2021-08-05 19:57:17 +0530

    AB#10326 added missing module under main.tf at moduel 'storage'

--- Deployment/src/main.tf
+++ Deployment/src/main.tf
@@ -14,6 +14,7 @@ module "storage" {
   name                = "${local.service_name}ui${local.env_name}website"
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
+  allowed_ips         = var.allowed_ips
   tags                = local.tags
   env_name            = local.env_name
   m_spoke_subnet      = data.azurerm_hub_n_spoke_subnet.id
