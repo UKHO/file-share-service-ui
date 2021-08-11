c7774871af7447bcdbde9a659ee592b9422c0b9a
ravi102465 <ravi102465@mastek.com>
2021-08-11 17:26:19 +0530

    typo fix

--- Deployment/src/Modules/Storage/main.tf
+++ Deployment/src/Modules/Storage/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_storage_account" "storage" {
     index_document = "index.html"
   }
     network_rules {
-    default_action             = "deny"
+    default_action             = "Deny"
     ip_rules                   = var.allowed_ips
     bypass                     = ["Logging", "Metrics", "AzureServices"]
     virtual_network_subnet_ids = [var.hub_n_spoke_subnet,var.agent_subnet]
