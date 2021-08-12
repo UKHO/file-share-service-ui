8f27262f433c6765e4e725c374b27fd755fd4ac8
Kajal Kundu <kajalk@mastek.com>
2021-08-12 23:07:31 +0100

    10372-fixed failed tests AB#10372

--- Tests/AccessibilityTests/accessibility-fss-ui-search-page.spec.ts
+++ Tests/AccessibilityTests/accessibility-fss-ui-search-page.spec.ts
@@ -2,9 +2,7 @@ import { chromium, BrowserContext, Browser, Page } from 'playwright'
 import { injectAxe, getViolations } from 'axe-playwright'
 const { autoTestConfig } = require('../FunctionalTests/appSetting.json');
 const { pageObjectsConfig, pageTimeOut } = require('../FunctionalTests/pageObjects.json');
-import { join } from 'path'
-import { SearchAttribute } from '../FunctionalTests/helpermethod'
-let name: string;
+import { SearchAttribute,LoginPortal } from '../FunctionalTests/helpermethod'
 
 let browser: Browser
 let context: BrowserContext;
@@ -12,48 +10,17 @@ let page: Page
 
 describe('FSS UI Search Page Accessibility Test Scenarios', () => {
   jest.setTimeout(pageTimeOut.timeOutInMilliSeconds);
-  beforeEach(async () => {
-    browser = await chromium.launch({ slowMo: 100, headless: false })
+  beforeAll(async () => {
+    browser = await chromium.launch({ slowMo: 100})
     context = await browser.newContext();
     page = await context.newPage();
     page.setDefaultTimeout(pageTimeOut.timeOutInMilliSeconds)
     await page.goto(autoTestConfig.url)
     page.click(pageObjectsConfig.loginSignInLinkSelector);
-    await LoginPortal(autoTestConfig.user, autoTestConfig.password);
-  })
+    await LoginPortal(page,autoTestConfig.user, autoTestConfig.password);
+  })  
 
-  //Function to sign in portal
-  //==================START==============================
-  async function LoginPortal(username: string, password: string) {
-
-    const [popup] = await Promise.all([
-      page.waitForEvent('popup')
-    ]);
-    try {
-      popup.setDefaultTimeout(pageTimeOut.timeOutInMilliSeconds);
-      popup.setViewportSize({ 'width': 800, 'height': 1024 })
-      await popup.waitForSelector(pageObjectsConfig.loginPopupSignInEmailSelector)
-      popup.fill(pageObjectsConfig.loginPopupSignInEmailSelector, username)
-      await popup.waitForSelector(pageObjectsConfig.loginPopupNextButtonSelector)
-      popup.click(pageObjectsConfig.loginPopupNextButtonSelector)
-      await popup.waitForSelector(pageObjectsConfig.loginPopupSignInPasswordSelector)
-      popup.fill(pageObjectsConfig.loginPopupSignInPasswordSelector, password)
-      await popup.waitForTimeout(2000);
-      popup.keyboard.press('Enter');  
-      await popup.waitForTimeout(2000);
-    }
-    catch (e) {
-      const errorPath = name.replace(" ", "") + "failedtest.jpeg"
-      await popup.screenshot({
-        path: join("screenshot", errorPath)
-      });
-      console.log(join(errorPath))
-    }
-
-
-  }
-  name = 'should return no violation for add rows  element'
-  test(name, async () => {
+  test('should return no violation for add rows element', async () => {
     await page.waitForSelector(pageObjectsConfig.searchPageContainerHeaderSelector);
     await injectAxe(page);
     const violations = await getViolations(page, '.addNewLine', {
@@ -70,15 +37,19 @@ describe('FSS UI Search Page Accessibility Test Scenarios', () => {
     expect(violations.length).toBe(0);
   })
 
-  test('should return no violation for Attributes dropdown element', async () => {
-    await page.waitForSelector(pageObjectsConfig.searchPageContainerHeaderSelector);
-    page.waitForSelector("#ukho-form-field-2")
-    await injectAxe(page);
-    const violations = await getViolations(page, '#ukho-form-field-2', {
+  test('should return no violation for search result table', async () => {
+    page.waitForTimeout(pageTimeOut.delay);
+    await page.waitForSelector("#ukho-form-field-3");
+    await SearchAttribute(page, "BusinessUnit");
+    await page.selectOption("#ukho-form-field-2", "eq");
+    await page.fill("#ukho-form-field-4", "adds");
+    await page.click("//button[text()='Search']");
+    page.waitForTimeout(pageTimeOut.delay);
+    await page.waitForSelector(".attribute-table");
+    const violations = await getViolations(page, '.attribute-table', {
       axeOptions: {
         runOnly: {
-          type: 'tag',
-          values: ['wcag2aa'],
+          type: 'tag', values: ['wcag2aa'],
         },
       },
     })
@@ -88,8 +59,8 @@ describe('FSS UI Search Page Accessibility Test Scenarios', () => {
     expect(violations.length).toBe(0);
   })
 
-  test('should return no violation for Operator dropdown element', async () => {
-    await page.waitForSelector(pageObjectsConfig.searchPageContainerHeaderSelector);
+  test('should return no violation for Attributes dropdown element', async () => {
+    
     page.waitForSelector("#ukho-form-field-3")
     await injectAxe(page);
     const violations = await getViolations(page, '#ukho-form-field-3', {
@@ -106,8 +77,26 @@ describe('FSS UI Search Page Accessibility Test Scenarios', () => {
     expect(violations.length).toBe(0);
   })
 
+  test('should return no violation for Operator dropdown element', async () => {
+    
+    page.waitForSelector("#ukho-form-field-2")
+    await injectAxe(page);
+    const violations = await getViolations(page, '#ukho-form-field-2', {
+      axeOptions: {
+        runOnly: {
+          type: 'tag',
+          values: ['wcag2aa'],
+        },
+      },
+    })
+    if (violations.length > 0) {
+      console.log(violations);
+    }
+    expect(violations.length).toBe(0);
+  })
+
   test('should return no violation for value inputbox element', async () => {
-    await page.waitForSelector(pageObjectsConfig.searchPageContainerHeaderSelector);
+    
     page.waitForSelector("#ukho-form-field-4")
     await injectAxe(page);
     const violations = await getViolations(page, '#ukho-form-field-4', {
@@ -122,30 +111,9 @@ describe('FSS UI Search Page Accessibility Test Scenarios', () => {
       console.log(violations);
     }
     expect(violations.length).toBe(0);
-  })
+  }) 
 
-  test('should return no violation for search result table', async () => {
-    await page.waitForSelector(pageObjectsConfig.searchPageContainerHeaderSelector);
-    await page.waitForSelector("#ukho-form-field-3");
-    await SearchAttribute(page, "BusinessUnit");
-    await page.selectOption("#ukho-form-field-2", "eq");
-    await page.fill("#ukho-form-field-4", "adds");
-    await page.click("//button[text()='Search']");
-    await page.waitForSelector(".attribute-table");
-    const violations = await getViolations(page, '.attribute-table', {
-      axeOptions: {
-        runOnly: {
-          type: 'tag', values: ['wcag2aa'],
-        },
-      },
-    })
-    if (violations.length > 0) {
-      console.log(violations);
-    }
-    expect(violations.length).toBe(0);
-  })
-
-  afterEach(async () => {
+  afterAll(async () => {
     await page.close();
     await context.close();
     await browser.close();
