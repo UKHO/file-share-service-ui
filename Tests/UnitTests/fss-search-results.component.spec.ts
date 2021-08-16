a74cff7c791bd9fbd3f084e565f1da914381fd4f
usha13529 <usha13529@Mastek.com>
2021-08-16 19:36:33 +0530

    AB# 10403 - Added unit test cases to cover search result components functionality

--- Tests/UnitTests/fss-search-results.component.spec.ts
+++ Tests/UnitTests/fss-search-results.component.spec.ts
@@ -1,17 +1,20 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ElementRef, NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 
-import { FssSearchResultsComponent } from '../../src/app/features/fss-search/fss-search-results/fss-search-results.component';
+import { formatBytes, FssSearchResultsComponent } from '../../src/app/features/fss-search/fss-search-results/fss-search-results.component';
 import { FileShareApiService } from '../../src/app/core/services/file-share-api.service';
 import { AppConfigService } from '../../src/app/core/services/app-config.service';
 import { MsalService, MSAL_INSTANCE } from '@azure/msal-angular';
 import { PublicClientApplication } from '@azure/msal-browser';
+import { BatchFileDetailsColumnData } from 'src/app/core/models/fss-search-results-types';
 
 describe('FssSearchResultsComponent', () => {
   let component: FssSearchResultsComponent;  
   let fileShareApiService: FileShareApiService;  
-  let msalService: MsalService
+  let msalService: MsalService;
+  let elementRef: ElementRef;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientModule,        ],
@@ -32,11 +35,112 @@ describe('FssSearchResultsComponent', () => {
     msalService = TestBed.inject(MsalService)
   });
 
-  it('should create', () => {
+  it('should create FssSearchResultsComponent', () => {
     const fixture = TestBed.createComponent(FssSearchResultsComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  //Test for search result count
+  test('should return search result count 1 when search result for 1 batch is provided', () => {
+    component = new FssSearchResultsComponent(elementRef, fileShareApiService);    
+    component.searchResult = Array.of(SearchResultMockData['entries']);    
+    component.ngOnChanges();   
+    var batches = component.searchResult[0];   
+
+    expect(batches.length).toEqual(1);
+    expect(component.searchResultVM.length).toEqual(1);    
+  });
+
+  //Test for batch attributes
+  test('should return batch attributes when search result data is provided', () => {
+    component = new FssSearchResultsComponent(elementRef, fileShareApiService);    
+    component.searchResult =Array.of(SearchResultMockData['entries']);
+    component.ngOnChanges();   
+    var batches = component.searchResult[0];    
+
+    var expectedBatchAttributes = [
+      {  "key": "product", "value": "TidalPredictionService" },
+      {  "key": "cellname", "value": "AVCS" }];
+
+    var batchAttributes = component.getBatchAttributes(batches[0]);
+
+    expect(batchAttributes.length).toEqual(2);
+    expect(batchAttributes).toEqual(expectedBatchAttributes);    
+  });
+
+  //Test for system attributes
+  test('should return system attributes when search result data is provided', () => {
+    component = new FssSearchResultsComponent(elementRef, fileShareApiService);    
+    component.searchResult =Array.of(SearchResultMockData['entries']);
+    component.ngOnChanges();         
+
+    var expectedBatchID = { "key": "Batch ID", "value": "9439e409-e545-435c-afd7-f3a5cce527e3" };
+    var expectedBatchPublishedDate =  { key: 'Batch published date', value: '2021-06-18T12:57:48.853Z' };
+    var expectedExpiryDate =  { key: 'Batch expiry date', value: '2022-02-28T13:05:10.14Z' };
+    
+    var batchID = component.searchResultVM[0].BatchID;
+    var batchPublishedDate = component.searchResultVM[0].BatchPublishedDate;
+    var expiryDate = component.searchResultVM[0].ExpiryDate;
+        
+    expect(batchID).toEqual(expectedBatchID);    
+    expect(batchPublishedDate).toEqual(expectedBatchPublishedDate); 
+    expect(expiryDate).toEqual(expectedExpiryDate); 
+  });
+
+  //Test for file details column headers
+  test('should return file details column data', () => {
+    component = new FssSearchResultsComponent(elementRef, fileShareApiService);
+    component.ngOnChanges();   
+
+    var expectedColumnData = ColumnHeader;
+    var resultedColumnData = component.getfileDetailsColumnData();
+
+    expect(expectedColumnData).toEqual(resultedColumnData);
+  });  
+
+  //Test for batch file details
+  test('should return batch file details when search result data is provided', () => {
+    component = new FssSearchResultsComponent(elementRef, fileShareApiService);    
+    component.searchResult =Array.of(SearchResultMockData['entries']);
+    component.ngOnChanges();   
+    var batches = component.searchResult[0];    
+
+    var expectedBatchFileDetails =  {
+      columnData: ColumnHeader,
+      rowData: [
+        {
+          FileName: 'My Test File.txt',
+          MimeType: 'image/jpeg',
+          FileSize: '3.81 MB',
+          Download: '<div class="fileDownload" rel="/batch/9439e409-e545-435c-afd7-f3a5cce527e3/files/My%20Test%20File.txt" tabindex="0" role="button" aria-label="Download File"><i class="fa fa-download fa-1x"></i></div>'
+        }
+      ]
+    };
+
+    var batchfileDetails = component.getBatchFileDetails(batches[0]);    
+
+    expect(batchfileDetails.rowData.length).toEqual(1);
+    expect(batchfileDetails).toEqual(expectedBatchFileDetails);    
+  });
+
+  //Test for file size conversion 
+  test('should convert file size from bytes to respective size units', () => {
+    component = new FssSearchResultsComponent(elementRef, fileShareApiService);
+    component.ngOnChanges();    
+    var zeroBytes = formatBytes(0);
+    var fileSizeBytes = formatBytes(100);
+    var fileSizeKB = formatBytes(2000);
+    var fileSizeMB = formatBytes(2500000);    
+    var fileSizeGB = formatBytes(3400000000);
+
+    expect(zeroBytes).toEqual('0 B');
+    expect(fileSizeBytes).toEqual('100 B');
+    expect(fileSizeKB).toEqual('1.95 KB');
+    expect(fileSizeMB).toEqual('2.38 MB');
+    expect(fileSizeGB).toEqual('3.17 GB');    
+  });    
+
 });
 
 export function MockMSALInstanceFactory () {    
@@ -55,3 +159,64 @@ export function MockMSALInstanceFactory () {
      }
    })           
  };
+
+ export const SearchResultMockData: any = { 
+    "count": 1,
+    "total": 1,
+    "entries": [
+        {
+            "batchId": "9439e409-e545-435c-afd7-f3a5cce527e3",
+            "status": "Committed",
+            "attributes": [
+                {
+                    "key": "product",
+                    "value": "TidalPredictionService"
+                },
+                {
+                    "key": "cellname",
+                    "value": "AVCS"
+                }
+            ],
+            "businessUnit": "TEST",
+            "batchPublishedDate": "2021-06-18T12:57:48.853Z",
+            "expiryDate": "2022-02-28T13:05:10.14Z",
+            "files": [
+                {
+                    "filename": "My Test File.txt",
+                    "fileSize": 4000000,
+                    "mimeType": "image/jpeg",
+                    "hash": null,
+                    "attributes": [
+                        {
+                            "key": "filetype",
+                            "value": "Country Delight"
+                        }
+                    ],
+                    "links": {
+                        "get": {
+                            "href": "/batch/9439e409-e545-435c-afd7-f3a5cce527e3/files/My%20Test%20File.txt"
+                        }
+                    }
+                }
+            ]
+        }
+    ],
+    "_Links": {
+        "self": {
+            "href": "/batch?limit=100&start=0&$filter=filesize%20gt%201000"
+        },
+        "first": {
+            "href": "/batch?limit=100&start=0&$filter=filesize%20gt%201000"
+        },
+        "last": {
+            "href": "/batch?limit=100&start=0&$filter=filesize%20gt%201000"
+        }
+    }
+ };
+
+ export const ColumnHeader:BatchFileDetailsColumnData[] = [
+  { headerTitle: 'File name', propertyName: 'FileName' },
+  { headerTitle: 'MIME type', propertyName: 'MimeType' },
+  { headerTitle: 'File size', propertyName: 'FileSize' },
+  { headerTitle: 'Download', propertyName: 'Download' }
+];
