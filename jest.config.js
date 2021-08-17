3458e257484e98a3e13f64d0babb8ab170972151
Chaitali Shidid <chaital14503@mastek.com>
2021-08-17 16:23:16 +0530

    10403-wip-excluding service files for code coverage improvement-AB#10403 AB#11013

--- jest.config.js
+++ jest.config.js
@@ -15,5 +15,6 @@ module.exports = {
   }),
   globals: {
     crypto: require('crypto')
-  }
+  },
+  testPathIgnorePatterns: ['<rootDir>/src/app/core/services/app-config.service.ts','<rootDir>/src/app/core/services/file-share-api.service.ts']
 };
