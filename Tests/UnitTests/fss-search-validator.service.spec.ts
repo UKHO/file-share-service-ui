4c319a19fe46476560c1d841f831297dc0d2c4d6
Chaitali Shidid <chaital14503@mastek.com>
2021-08-18 15:34:10 +0530

    10403-unit test cases for validator service-AB#10403 AB#11101

--- Tests/UnitTests/fss-search-validator.service.spec.ts
+++ Tests/UnitTests/fss-search-validator.service.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { FormControl } from '@angular/forms';
+import { Field, FssSearchRow, Operator } from '../../src/app/core/models/fss-search-types';
 
 import { FssSearchValidatorService } from '../../src/app/core/services/fss-search-validator.service';
 
@@ -13,4 +15,63 @@ describe('FssSearchValidatorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  test('should highlight value field when FileSize value is not entered', () => {
+    let searchRows: FssSearchRow[] = [];
+    let frmControl = new FormControl();
+    let formControlTime = new FormControl();
+    searchRows.push(createSearchRow(2, getFields(), getOperators(), 'OR', 'FileSize', 'eq', '', 'tel', false, frmControl, formControlTime, ''));
+    console.log(searchRows);
+    service.validateValueFormControl(searchRows, getFields(), getOperators());
+    expect(searchRows[0].valueFormControl.touched).toEqual(true);
+  });
+
+  test('should return datatype as "date" when SystemAttribute BatchExpiryDate is passed', () => {
+    expect(service.getFieldDataType('ExpiryDate', getFields())).toEqual('date');
+  });
+
+  test('should highlight value field when BatchExpiryDate value is not entered when non null operator is selected', () => {
+    let searchRows: FssSearchRow[] = [];
+    let frmControl = new FormControl();
+    let formControlTime = new FormControl();
+    searchRows.push(createSearchRow(2, getFields(), getOperators(), 'OR', 'ExpiryDate', 'eq', '', 'date', false, frmControl, formControlTime, ''));
+    console.log(searchRows);
+    service.validateValueFormControl(searchRows, getFields(), getOperators());
+    expect(searchRows[0].valueFormControl.touched).toEqual(true);
+  });
 });
+
+export function createSearchRow(rowId: number, fields: Field[], operators: Operator[], joinOperator: string, field: string, operator: string, value: any, valueType: "time" | "text" | "date" | "email" | "password" | "tel" | "url", isValueHidden: boolean, formControl: FormControl, formControlTime: FormControl, time: string) {
+  var row = new FssSearchRow();
+  row.rowId = rowId;
+  row.fields = fields,
+    row.operators = operators;
+  row.selectedJoinOperator = joinOperator;
+  row.selectedField = field;
+  row.selectedOperator = operator;
+  row.value = value;
+  row.valueType = valueType;
+  row.isValueHidden = isValueHidden;
+  row.valueFormControl = formControl;
+  row.valueFormControlTime = formControlTime;
+  row.time = time;
+  return row;
+}
+
+export function getFields() {
+  let fields: Field[] = [
+    { value: 'FileName', text: '@FileName', type: 'SystemAttribute', dataType: 'string' },
+    { value: 'FileSize', text: '@FileSize', type: 'SystemAttribute', dataType: 'number' },
+    { value: 'ExpiryDate', text: '@BatchExpiryDate', type: 'SystemAttribute', dataType: 'date' },
+  ];
+  return fields;
+}
+
+export function getOperators() {
+  let operators: Operator[] = [
+    { value: 'eq', text: '=', type: 'operator', supportedDataTypes: ['string', 'number', 'date', 'attribute'] },
+    { value: 'gt', text: '>', type: 'operator', supportedDataTypes: ['number', 'date'] },
+    { value: 'le', text: '<=', type: 'operator', supportedDataTypes: ['number', 'date'] }
+  ];
+  return operators;
+}
