a312837fdfffb1002b2419f4384e2782271c8d81
Shrikant Gautam <Shrikan14443@Mastek.Com>
2021-09-02 15:21:08 +0530

    11256-code restructing and code optimization AB#11256 AB#11360

--- src/app/core/services/fss-popular-search.service.ts
+++ src/app/core/services/fss-popular-search.service.ts
@@ -10,41 +10,40 @@ export class FssPopularSearchService {
 
   getSearchQuery(fssSearchRows: FssSearchRow[], popularSearch: any, operators: Operator[]) {
     console.log("Before Value change", fssSearchRows, popularSearch);
-    for (let i = 0; i < fssSearchRows.length; i++) {
-      var popularSearchRow = popularSearch.rows[i]
-      fssSearchRows[i].selectedField = popularSearchRow.field;
-      fssSearchRows[i].selectedJoinOperator = popularSearchRow.andOr;
-      fssSearchRows[i].group = popularSearchRow.group;
-      fssSearchRows[i].selectedOperator = popularSearchRow.operator;
-      var fieldDataType = this.getFieldDataType(fssSearchRows[i].selectedField, fssSearchRows[i].fields);
-      fssSearchRows[i].operators = this.getFilteredOperators(fieldDataType, operators);
+    for (let rowIndex = 0; rowIndex < fssSearchRows.length; rowIndex++) {
+      var popularSearchRow = popularSearch.rows[rowIndex];
+      var fssSearchRow = fssSearchRows[rowIndex];
+      fssSearchRow.selectedField = popularSearchRow.field;
+      fssSearchRow.selectedJoinOperator = popularSearchRow.andOr;
+      fssSearchRow.group = popularSearchRow.group;
+      fssSearchRow.selectedOperator = popularSearchRow.operator;
+
+      var fieldDataType = this.getFieldDataType(fssSearchRow.selectedField, fssSearchRow.fields);
+      fssSearchRow.operators = this.getFilteredOperators(fieldDataType, operators);
+      fssSearchRow.valueType = this.getValueType(fieldDataType);
+
       if (popularSearchRow.isDynamicValue) {
         popularSearchRow.value = popularSearchRow.value.replace(/^"(.*)"$/, '$1');
-        var value = eval(popularSearchRow.value)
-        var dateTime = this.getDateTime(value);
-        fssSearchRows[i].value = dateTime[0];
-        fssSearchRows[i].time = dateTime[1];
+        var value = eval(popularSearchRow.value);
+        this.getDateTime(value, fssSearchRow);
       }
       else {
-        fssSearchRows[i].value = popularSearchRow.value;
+        fssSearchRow.value = popularSearchRow.value;
       }
-      fssSearchRows[i].valueType = this.getValueType(fieldDataType);
     }
     console.log("After Value change", fssSearchRows, popularSearch);
   }
 
-  getDateTime(value: any) {
-    var month = value.getMonth() + 1;
-    var getDate = value.getDate();
-    var hours = value.getHours();
-    var minutes = value.getMinutes();
-    month = (month < 10) ? ("0" + month) : month;
-    getDate = (getDate < 10) ? ("0" + getDate) : getDate;
-    hours = (hours < 10) ? ("0" + hours) : hours;
-    minutes = (minutes < 10) ? ("0" + minutes) : minutes;
-    var date = value.getFullYear()+ '-' + month + '-' + getDate;
-    var time = hours + ':' + minutes;
-    return [date, time];
+  getDateBeforeNDays(nDays:number, startHour:any, startMinutes:any){
+    var currentDate: any = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), startHour, startMinutes)
+    var startDate: any = new Date(nDays*24*60*60*1000)
+    var date = new Date(currentDate - startDate)
+    return date;
+  }
+
+  getDateTime(value: any, fssSearchRow: FssSearchRow) {
+    fssSearchRow.value = value.toLocaleDateString('fr-CA');
+    fssSearchRow.time = value.toLocaleTimeString('en-GB',{hour:'2-digit', minute:'2-digit'});
   }
 
   getFieldDataType(fieldValue: string, fields: Field[]) {
