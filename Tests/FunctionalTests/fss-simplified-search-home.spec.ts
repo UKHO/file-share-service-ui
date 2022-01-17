a65a693ca78760e79494b9d702baced6845d8edd
Kajal Kundu <kajalk@mastek.com>
2022-01-17 20:50:14 +0000

    added automated functional tests AB#12669

---
+++ Tests/FunctionalTests/fss-simplified-search-home.spec.ts
@@ -0,0 +1,82 @@
+import { chromium, Browser, BrowserContext, Page } from 'playwright'
+const { autoTestConfig } = require('./appSetting');
+const { pageObjectsConfig,pageTimeOut } = require('./pageObjects');
+import {LoginPortal,SearchAttribute} from './helpermethod'
+
+
+describe('Test Search Attribute Scenario On Search Page', () => {
+  jest.setTimeout(pageTimeOut.timeOutInMilliSeconds);
+  let browser: Browser;
+  let context: BrowserContext;
+  let page: Page;  
+
+  beforeAll(async () => {
+    browser = await chromium.launch({slowMo:100});
+    context = await browser.newContext();
+    page = await context.newPage();    
+    await page.goto(autoTestConfig.url)
+    await page.waitForTimeout(pageTimeOut.delay)
+    if((await page.$$(pageObjectsConfig.acceptCookieSelector)).length > 0){
+      await page.click(pageObjectsConfig.acceptCookieSelector);
+    }
+    page.click(pageObjectsConfig.searchButtonSelector);
+    await LoginPortal(page,autoTestConfig.user, autoTestConfig.password);    
+    
+    await page.waitForSelector(pageObjectsConfig.searchPageContainerHeaderSelector);
+    expect(await page.innerHTML(pageObjectsConfig.searchPageContainerHeaderSelector)).toEqual(pageObjectsConfig.searchPageContainerHeaderText);
+  })
+
+  afterAll(async () => {
+     await page.close()
+     await context.close()
+     await browser.close()
+  })
+
+  it('Does it display "Simplified Search" link on advanced Search page', async () => {
+    await page.waitForSelector(pageObjectsConfig.simplifiedSeatchLinkSelector);
+
+    var simplifiedSearchLink = await page.innerText(pageObjectsConfig.simplifiedSeatchLinkSelector);
+    expect(simplifiedSearchLink).toEqual(pageObjectsConfig.simplifiedSeatchLinkText);
+  })
+
+  it('User clicks on "Simplified Search" link navigates to Simplified Search page', async () => {
+    await page.click(pageObjectsConfig.simplifiedSeatchLinkSelector);
+    //Verify element in advance search page count should be zero after navigating to simplified search page
+    
+    var simplifiedSearchBox= (await page.$$(pageObjectsConfig.inputSimplifiedSerachBoxSelector)).length
+    expect(simplifiedSearchBox).toEqual(1);
+
+    var advanceSearchElements =(await page.$$("#ukho-form-field-2,#ukho-form-field-3,#ukho-form-field-4")).length
+    expect(advanceSearchElements).toEqual(0);
+  })
+
+  it('Does it display "Advanced Search" link on Simplified Search page', async () => {
+       
+    var advancedSearchLink = await page.innerText(pageObjectsConfig.advancedSeatchLinkSelector);
+    expect(advancedSearchLink).toEqual(pageObjectsConfig.advancedSeatchLink);
+
+  })
+
+  it('Does it display "Error message" if user clicks on serach button and simplified search box is empty', async () => {
+    await page.waitForSelector(pageObjectsConfig.inputSimplifiedSerachBoxSelector);
+    await page.click(pageObjectsConfig.simplifiedSearchButtonSelector);
+    var errorMessage = await page.innerText(pageObjectsConfig.dialogWarningSelector);
+    expect(errorMessage).toContain(pageObjectsConfig.warningMessageValue);
+  }) 
+
+  it('User clicks on "Advanced Search" link navigates to Advanced Search page', async () => {
+    await page.click(pageObjectsConfig.advancedSeatchLinkSelector);
+
+    await page.waitForSelector(pageObjectsConfig.inputSearchFieldSelector);
+
+    //Verify element in simplified search page count should be zero after navigating to advanced search page
+    
+    var advanceSearchElements =(await page.$$("#ukho-form-field-2,#ukho-form-field-3,#ukho-form-field-4")).length
+    expect(advanceSearchElements).toEqual(3);
+
+    var simplifiedSearchBox= (await page.$$(pageObjectsConfig.inputSimplifiedSerachBoxSelector)).length
+    expect(simplifiedSearchBox).toEqual(0);
+    
+  })
+
+})
