c640fe2d0c040a92da1dc98abc00c505bd64a959
Kajal Kundu <kajalk@mastek.com>
2022-01-18 15:58:47 +0000

    resolved review comments and uplifted variable name and typo error AB#12669

--- Tests/FunctionalTests/fss-simplified-search-home.spec.ts
+++ Tests/FunctionalTests/fss-simplified-search-home.spec.ts
@@ -31,19 +31,12 @@ describe('Test Search Attribute Scenario On Simplified Search Page', () => {
      await context.close()
      await browser.close()
   })
-
-  it('Does it display "Simplified Search" link on advanced Search page', async () => {
-    await page.waitForSelector(pageObjectsConfig.simplifiedSeatchLinkSelector);
-
-    var simplifiedSearchLink = await page.innerText(pageObjectsConfig.simplifiedSeatchLinkSelector);
-    expect(simplifiedSearchLink).toEqual(pageObjectsConfig.simplifiedSeatchLinkText);
-  })
-
+ 
   it('Verify user clicks on "Simplified Search" link navigates to Simplified Search page', async () => {
-    await page.click(pageObjectsConfig.simplifiedSeatchLinkSelector);
+    await page.click(pageObjectsConfig.simplifiedSearchLinkSelector);
     //Verify element in advance search page count should be zero after navigating to simplified search page
     
-    var simplifiedSearchBox= (await page.$$(pageObjectsConfig.inputSimplifiedSerachBoxSelector)).length
+    var simplifiedSearchBox= (await page.$$(pageObjectsConfig.inputSimplifiedSearchBoxSelector)).length
     expect(simplifiedSearchBox).toEqual(1);
 
     var advanceSearchElements =(await page.$$("#ukho-form-field-2,#ukho-form-field-3,#ukho-form-field-4")).length
@@ -52,20 +45,20 @@ describe('Test Search Attribute Scenario On Simplified Search Page', () => {
 
   it('Does it display "Advanced Search" link on Simplified Search page', async () => {
        
-    var advancedSearchLink = await page.innerText(pageObjectsConfig.advancedSeatchLinkSelector);
-    expect(advancedSearchLink).toEqual(pageObjectsConfig.advancedSeatchLink);
+    var advancedSearchLink = await page.innerText(pageObjectsConfig.advancedSearchLinkSelector);
+    expect(advancedSearchLink).toEqual(pageObjectsConfig.advancedSearchLink);
 
   })
 
-  it('Does it display "Error message" if user clicks on serach button and simplified search box is empty', async () => {
-    await page.waitForSelector(pageObjectsConfig.inputSimplifiedSerachBoxSelector);
+  it('Does it display "Error message" if user clicks on search button and simplified search box is empty', async () => {
+    await page.waitForSelector(pageObjectsConfig.inputSimplifiedSearchBoxSelector);
     await page.click(pageObjectsConfig.simplifiedSearchButtonSelector);
     var errorMessage = await page.innerText(pageObjectsConfig.dialogWarningSelector);
     expect(errorMessage).toContain(pageObjectsConfig.warningMessageValue);
   }) 
 
-  it('Veify user clicks on "Advanced Search" link navigates to Advanced Search page', async () => {
-    await page.click(pageObjectsConfig.advancedSeatchLinkSelector);
+  it('Verify user clicks on "Advanced Search" link navigates to Advanced Search page', async () => {
+    await page.click(pageObjectsConfig.advancedSearchLinkSelector);
 
     await page.waitForSelector(pageObjectsConfig.advancedSearchTableSelector);
 
@@ -75,7 +68,7 @@ describe('Test Search Attribute Scenario On Simplified Search Page', () => {
     expect(advanceSearchElements).toEqual(3);
     
     //search box for simplified search is not present on advanced search page.
-    var simplifiedSearchBox= (await page.$$(pageObjectsConfig.inputSimplifiedSerachBoxSelector)).length
+    var simplifiedSearchBox= (await page.$$(pageObjectsConfig.inputSimplifiedSearchBoxSelector)).length
     expect(simplifiedSearchBox).toEqual(0);
     
   })
