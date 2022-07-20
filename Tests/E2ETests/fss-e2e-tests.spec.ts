878c41aa219961ea1b29ec48594ba54381171f1d
Kishor Ambare <Kishor13892@Mastek.Com>
2022-07-20 17:16:43 +0530

    Upgrading 'e2e' tests with TestRunner

--- Tests/E2ETests/fss-e2e-tests.spec.ts
+++ Tests/E2ETests/fss-e2e-tests.spec.ts
@@ -1,114 +1,97 @@
-import { chromium, Browser, BrowserContext, Page } from 'playwright';
-const { autoTestConfig } = require('../FunctionalTests/appSetting.json');
-const { pageObjectsConfig, pageTimeOut } = require('../FunctionalTests/pageObjects.json');
-import { LoginPortal, SearchAttribute, ClickWaitRetry, AcceptCookies,
-  ExpectAllResultsHaveBatchUserAttValue} from '../FunctionalTests/helpermethod';
-import { attributeFileSize, attributeBusinessUnit, attributeProductType } from '../FunctionalTests/helperconstant';
-import { GetApiDetails } from './apiRequest';
+import { test, expect } from '@playwright/test';
+import { autoTestConfig } from '../../appSetting.json';
+import { attributeBusinessUnit, attributeProductType, attributeFileSize} from '../../Helper/ConstantHelper';
+import { SearchAttribute,ExpectAllResultsHaveBatchUserAttValue, ClickWaitRetry } from '../../Helper/SearchPageHelper';
+import { AcceptCookies,LoginPortal } from '../../Helper/CommonHelper';
+import { fssSearchPageObjectsConfig } from '../../PageObjects/fss-searchpageObjects.json';
+import { commonObjectsConfig } from '../../PageObjects/commonObjects.json';
 
-describe('FSS UI E2E Scenarios', () => {
-  jest.setTimeout(pageTimeOut.timeOutInMilliSeconds);
-  let browser: Browser;
-  let context: BrowserContext;
-  let page: Page;
+let idToken: string | null;
+test.describe('FSS UI E2E Scenarios', () => {
 
-  beforeAll(async () => {
-    browser = await chromium.launch({slowMo: 100});
-  })
+  test.beforeEach(async ({ page }) => {
 
-  beforeEach(async () => {
-    context = await browser.newContext();
-    page = await context.newPage();
     await page.goto(autoTestConfig.url);
     await AcceptCookies(page);
-    await LoginPortal(page, autoTestConfig.user, autoTestConfig.password, pageObjectsConfig.loginSignInLinkSelector);
+    page.waitForNavigation();
+    await LoginPortal(page, autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
 
-    await page.waitForSelector(pageObjectsConfig.searchPageContainerHeaderSelector);
-    expect(await page.innerHTML(pageObjectsConfig.searchPageContainerHeaderSelector))
-      .toEqual(pageObjectsConfig.searchPageContainerHeaderText);
-    await page.click(pageObjectsConfig.advancedSearchLinkSelector, {force: true});
-
-  })
-
-  afterEach(async () => {
-    await page.close();
-    await context.close();
-  })
-  afterAll(async () => {
-    await browser.close();
-  })
-
-  it('Valid search system attributes query to verify data returns on UI and API response status 200', async () => {
-    page.setDefaultTimeout(pageTimeOut.timeOutInMilliSeconds);
-    await SearchAttribute(page, attributeBusinessUnit.key);
-    await page.selectOption(pageObjectsConfig.operatorDropDownSelector, "eq");
-    await page.fill(pageObjectsConfig.inputSearchValueSelector, attributeBusinessUnit.value);
-
-    await ClickWaitRetry(page, pageObjectsConfig.searchAttributeButton, pageObjectsConfig.searchAttributeTable);
-
-    // Verification of attribute table records
-    const noOfRecods = (await page.$$(pageObjectsConfig.searchAttributeTableRows)).length;
-    expect(noOfRecods).toBeGreaterThanOrEqual(2);
-
+    await page.waitForSelector(fssSearchPageObjectsConfig.searchPageContainerHeaderSelector);
+    expect(await page.innerHTML(fssSearchPageObjectsConfig.searchPageContainerHeaderSelector))
+      .toEqual(fssSearchPageObjectsConfig.searchPageContainerHeaderText);
+    await page.click(fssSearchPageObjectsConfig.advancedSearchLinkSelector, { force: true });
     // Get the token from local storage once user logged in
-    const idToken = await page.evaluate(() => { return localStorage.getItem('idToken') });
+    idToken = await page.evaluate(() => { return localStorage.getItem('idToken') });
 
+  })
+
+  test('Valid search system attributes query to verify data returns on UI and API response status 200', async ({ request, page }) => {
+
+    page.setDefaultTimeout(4500000);
+    await SearchAttribute(page, attributeBusinessUnit.key);
+    await page.selectOption(fssSearchPageObjectsConfig.operatorDropDownSelector, "eq");
+    await page.fill(fssSearchPageObjectsConfig.inputSearchValueSelector, attributeBusinessUnit.value);
+    await ClickWaitRetry(page, fssSearchPageObjectsConfig.searchAttributeButton, fssSearchPageObjectsConfig.searchAttributeTable);
+    // Verification of attribute table records
+    const noOfRecods = (await page.$$(fssSearchPageObjectsConfig.searchAttributeTableRows)).length;
+    expect(noOfRecods).toBeGreaterThanOrEqual(2);
     // Search Query String
     const queryString = `${attributeBusinessUnit.key} eq '${attributeBusinessUnit.value}'`;
+    const testUrl = `${autoTestConfig.apiurl}/batch?$filter=${queryString}`;
+    //Get operation   
+    const apiResponse = await request.get(testUrl, {
+      headers: {
+        'Authorization': `Bearer ${idToken}`
+      }
+    });
+    //to check the status code
+    expect(apiResponse.status()).toBe(200);
+  });
 
-    // Validate api response status code matches 200 
-    const statusCode = await GetApiDetails(autoTestConfig.apiurl, queryString, idToken!);
-
-    expect(statusCode).toEqual(200);
-
-  })
-
-  it('Valid search user attributes query to verify data returns on UI and API response status 200', async () => {
-    page.setDefaultTimeout(pageTimeOut.timeOutInMilliSeconds);
+  test('Valid search user attributes query to verify data returns on UI and API response status 200', async ({ request, page }) => {
+    page.setDefaultTimeout(350000);
     await SearchAttribute(page, attributeProductType.key);
-    await page.selectOption(pageObjectsConfig.operatorDropDownSelector, "eq");
-    await page.fill(pageObjectsConfig.inputSearchValueSelector, attributeProductType.value);
-
-    await ClickWaitRetry(page, pageObjectsConfig.searchAttributeButton, pageObjectsConfig.searchAttributeTable);
+    await page.selectOption(fssSearchPageObjectsConfig.operatorDropDownSelector, "eq");
+    await page.fill(fssSearchPageObjectsConfig.inputSearchValueSelector, attributeProductType.value);
+    await ClickWaitRetry(page, fssSearchPageObjectsConfig.searchAttributeButton, fssSearchPageObjectsConfig.searchAttributeTable);
     await ExpectAllResultsHaveBatchUserAttValue(page, attributeProductType.value);
-
-    // Get the token from local storage once user logged in
-    const idToken = await page.evaluate(() => { return localStorage.getItem('idToken') });
-
     // Search Query String
     const queryString = `$batch("${attributeProductType.key}") eq '${attributeProductType.value}'`;
-
-    // Validate api response status code matches 200 
-    var statusCode = await GetApiDetails(autoTestConfig.apiurl, queryString, idToken!);
-
-    expect(statusCode).toEqual(200);
+    const testUrl = `${autoTestConfig.apiurl}/batch?$filter=${queryString}`;
+    
+    //Get operation   
+    const apiResponse = await request.get(testUrl, {
+      headers: {
+        'Authorization': `Bearer ${idToken}`
+      }
+    });
+    //to check the status code
+    expect(apiResponse.status()).toBe(200);
 
   })
 
-  it('Invalid search query to verify data returns on UI and API response status 400', async () => {
-    page.setDefaultTimeout(pageTimeOut.timeOutInMilliSeconds);
+  test('Invalid search query to verify data returns on UI and API response status 400', async ({ page, request }) => {
+    page.setDefaultTimeout(200000);
     await SearchAttribute(page, attributeFileSize.key);
-    await page.selectOption(pageObjectsConfig.operatorDropDownSelector, "eq");
-    await page.fill(pageObjectsConfig.inputSearchValueSelector, `'${attributeFileSize.value}'`);
+    await page.selectOption(fssSearchPageObjectsConfig.operatorDropDownSelector, "eq");
+    await page.fill(fssSearchPageObjectsConfig.inputSearchValueSelector, `'${attributeFileSize.value}'`);
     await page.waitForTimeout(2000);
-
-    await ClickWaitRetry(page, pageObjectsConfig.searchAttributeButton, pageObjectsConfig.warningMessageSelector);
+    await ClickWaitRetry(page, fssSearchPageObjectsConfig.searchAttributeButton, fssSearchPageObjectsConfig.warningMessageSelector);
 
     //Verification of warning message
-    const warningMessage = await page.innerText(pageObjectsConfig.warningMessageSelector);
-
-    expect(warningMessage).toContain(pageObjectsConfig.warningMessageText);
-
-    //Get the token from local storage once user logged in
-    const idToken = await page.evaluate(() => { return localStorage.getItem('idToken') })
-
+    const warningMessage = await page.innerText(fssSearchPageObjectsConfig.warningMessageSelector);
+    expect(warningMessage).toContain(fssSearchPageObjectsConfig.warningMessageText);
     //Search Query String
     const queryString = `${attributeFileSize.key} eq '${attributeFileSize.value}'`;
-
-    //Validate api response status code matches 400 
-    var statusCode = await GetApiDetails(autoTestConfig.apiurl, queryString, idToken!);
-
-    expect(statusCode).toEqual(400);
-
+    const testUrl = `${autoTestConfig.apiurl}/batch?$filter=${queryString}`;
+    
+    //Get operation   
+    const apiResponse = await request.get(testUrl, {
+      headers: {
+        'Authorization': `Bearer ${idToken}`
+      }
+    });
+    //to check the status code
+    expect(apiResponse.status()).toBe(400);
   })
 })
