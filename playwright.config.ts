778b228a81f3dcf6d8bb2c64d35ab985099627e7
Kishor Ambare <Kishor13892@Mastek.Com>
2022-07-21 12:45:42 +0530

    Updated playwright config to remove unwanted lines.

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,15 +2,6 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 import { autoTestConfig } from './appSetting.json';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 const config: PlaywrightTestConfig = {
   
   retries: 3,
@@ -37,13 +28,6 @@ const config: PlaywrightTestConfig = {
   use: {
     actionTimeout: 80 * 1000,
     navigationTimeout: 150 * 1000,
-
-   // baseURL: "https://filesqa.admiralty.co.uk/batch?limit=10&start=0&$filter=$batch(ProductCode) eq 'AVCS' and $batch(CellName) eq 'CA479226' and $batch(EditionNumber) eq '4' and $batch(UpdateNumber) eq '6'",
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -77,38 +61,11 @@ const config: PlaywrightTestConfig = {
         channel: 'msedge'
       }
     }
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge',
-    //   },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: {
-    //     channel: 'chrome',
-    //   },
-    // },
+   
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
+  outputDir: 'test-results/',
 
   /* Run your local dev server before starting the tests */
   // webServer: {
