adf05c6c587ecf61f48d4565e6f82010c722a1f8
Chaitali Shidid <chaital14503@mastek.com>
2022-07-26 13:01:53 +0530

    13017 - file upload - component and service added AB#13017

--- src/app/features/exchange-set/exchange-set.module.ts
+++ src/app/features/exchange-set/exchange-set.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ExchangeSetRoutingModule } from './exchange-set-routing.module';
 import { ExchangeSetComponent } from './exchange-set.component';
+import { EssUploadFileComponent } from './ess-upload-file/ess-upload-file/ess-upload-file.component';
 
 
 @NgModule({
   declarations: [
-    ExchangeSetComponent
+    ExchangeSetComponent,
+    EssUploadFileComponent
   ],
   imports: [
     CommonModule,
