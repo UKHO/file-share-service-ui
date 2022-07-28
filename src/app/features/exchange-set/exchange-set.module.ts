98eb5042c7ed03bb45e2aea2e6f499f37f0bb09c
Prasad Rasal <Prasad14878@Mastek.Com>
2022-07-28 15:13:26 +0530

    Modified the coding AB#13016

--- src/app/features/exchange-set/exchange-set.module.ts
+++ src/app/features/exchange-set/exchange-set.module.ts
@@ -5,7 +5,8 @@ import { ExchangeSetRoutingModule } from './exchange-set-routing.module';
 import { ExchangeSetComponent } from './exchange-set.component';
 import { EssUploadFileComponent } from './ess-upload-file/ess-upload-file/ess-upload-file.component';
 import { EssAddSingleEncsComponent } from './ess-add-single-encs/ess-add-single-encs.component';
-import { RadioModule,TextinputModule,ButtonModule,FileInputModule } from '@ukho/design-system';
+import { RadioModule,TextinputModule,ButtonModule,FileInputModule,RadioGroupModule } from '@ukho/design-system';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,9 @@ import { RadioModule,TextinputModule,ButtonModule,FileInputModule } from '@ukho/
     RadioModule,
     TextinputModule,
     ButtonModule,
-    FileInputModule
+    FileInputModule,
+    RadioGroupModule,
+    FormsModule
   ]
 })
 export class ExchangeSetModule { }
