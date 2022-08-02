fa10fe2fbe6c5ee86e8d5080eec58f9676ad1a20
Amogh Lanke <amogh.lanke@mastek.conm>
2022-08-02 19:53:13 +0530

    AB#13616 test changes

---
+++ Tests/AccessibilityTests/jest.config.js
@@ -0,0 +1,20 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+  preset: 'jest-preset-angular',
+  roots: ['<rootDir>/Tests/UnitTests/'],
+  testMatch: ['**/+(*.)+(spec).+(ts)'],
+  setupFilesAfterEnv:  ['<rootDir>/setup-jest.ts'],
+  collectCoverage: true,
+  coverageReporters: ['html','cobertura'],
+  coverageDirectory: 'coverage',
+  reporters: ["default","jest-junit"],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, {
+    prefix: '<rootDir>/'
+  }),
+  globals: {
+    crypto: require('crypto')
+  },
+  coveragePathIgnorePatterns: ['<rootDir>/src/app/core/services/app-config.service.ts','<rootDir>/src/app/core/services/file-share-api.service.ts']
+};
