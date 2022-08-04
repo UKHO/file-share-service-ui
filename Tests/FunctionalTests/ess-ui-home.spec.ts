7bb415563cc3e418c99a966e5135ad56056cac4b
Kishor Ambare <Kishor13892@Mastek.Com>
2022-08-04 18:57:47 +0530

    AB#13729-Removed one assertion as per suggestion by Ravi

--- Tests/FunctionalTests/ess-ui-home.spec.ts
+++ Tests/FunctionalTests/ess-ui-home.spec.ts
@@ -16,44 +16,43 @@ test.describe('ESS UI Home Page Functional Test Scenarios', ()=>{
         await AcceptCookies(page);   
         })
 
-    test('Veriy "Search" link is not available on home page before login', async ({ page }) => {
+    test('Verify "Search" link is not available on home page before login', async ({ page }) => {
         expect (await page.isHidden (fssHomePageObjectsConfig.searchLinkSelector)).toBeTruthy();
       
     });
 
-    test('Veriy "Exchange sets" link is not available on home page before login', async ({ page }) => {
+    test('Verify "Exchange sets" link is not available on home page before login', async ({ page }) => {
         expect (await page.isHidden (fssHomePageObjectsConfig.essLinkSelector)).toBeTruthy();          
     });
 
-    test('Veriy clicking Search link from home page after login, navigates to Search page', async ({ page }) => {
+    test('Verify clicking Search link from home page after login, navigates to Search page', async ({ page }) => {
         await LoginPortal(page,autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
         await page.locator(fssHomePageObjectsConfig.searchLinkSelector).click();
         await page.waitForLoadState('load');
+        await expect(page).toHaveTitle("Admiralty - File Share Service - Search");
         expect(await page.innerHTML(fssSearchPageObjectsConfig.searchPageContainerHeaderSelector)).toEqual(fssSearchPageObjectsConfig.searchPageContainerHeaderText);      
     });
 
-    test('Veriy clicking Exchange sets link after login navigates to ESS laanding page', async ({ page }) => {
+    test('Verify clicking Exchange sets link after login navigates to ESS laanding page', async ({ page }) => {
         await LoginPortal(page,autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
         await page.locator(fssHomePageObjectsConfig.essLinkSelector).click();
-        await page.waitForLoadState('load');
-       // await expect(page).toHaveURL("https://fss-dev.admiralty.co.uk/#/exchangesets");
-       await expect(page).toHaveTitle("Admiralty - File Share Service - Exchange Sets"); 
-        expect(await page.innerText(esslandingpageObjectsConfig.radioButtonNameSelector)).toEqual(esslandingpageObjectsConfig.radioButtonName);
+        await page.waitForTimeout(500);
+        await expect(page.url()).toContain("/exchangesets");
+        await expect(page).toHaveTitle("Admiralty - File Share Service - Exchange Sets");        
     });
 
-    test('Veriy appending "/search" in  url after login, navigates to Search page', async ({ page }) => {
+    test('Verify appending "/search" in  url after login, navigates to Search page', async ({ page }) => {
         await LoginPortal(page,autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
-        await page.goto(autoTestConfig.url+"#"+"/search");
+        await page.goto(autoTestConfig.url+"/#"+"/search");
         await page.waitForLoadState('load');
         expect(await page.innerHTML(fssSearchPageObjectsConfig.searchPageContainerHeaderSelector)).toEqual(fssSearchPageObjectsConfig.searchPageContainerHeaderText); 
       
     });
 
-    test('Veriy appending "/exchangesets" in  url after login, navigates to ESS laanding page', async ({ page }) => {
+    test('Verify appending "/exchangesets" in  url after login, navigates to ESS laanding page', async ({ page }) => {
         await LoginPortal(page,autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
         await page.goto(autoTestConfig.url+"/#"+"/exchangesets");
         await page.waitForLoadState('load');
-        await page.waitForTimeout(3000);
         expect(await page.innerText(esslandingpageObjectsConfig.radioButtonNameSelector)).toEqual(esslandingpageObjectsConfig.radioButtonName);
     });    
 });
