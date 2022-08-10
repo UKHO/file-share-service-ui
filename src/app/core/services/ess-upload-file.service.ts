06e9745fc92463dbbe9ca493b9e36df97fd8e0b0
Suraj Singh <suraj49470@gmail.com>
2022-08-10 15:15:51 +0530

    addSelectedEnc spread operator added

--- src/app/core/services/ess-upload-file.service.ts
+++ src/app/core/services/ess-upload-file.service.ts
@@ -12,6 +12,8 @@ export class EssUploadFileService {
   constructor() {
     this.selectedEncs = [];
     this.maxEncLimit = AppConfigService.settings['essConfig'].MaxEncLimit;
+    this.validEncs =  ['AU210130', 'AU210140', 'AU220130', 'AU220150', 'AU314128'];
+  
   }
 
   validateCSVFile() {}
@@ -73,7 +75,7 @@ export class EssUploadFileService {
   }
 
   addSelectedEnc(enc: string): void{
-    this.selectedEncs.push(enc);
+    this.selectedEncs = [...this.selectedEncs , enc];
   }
 
   removeSelectedEnc(enc: string): void{
