3bd59fabb82c6af2c81f3d0b7709e54d717c0230
Sheetal Dalavi <sheetal15692@mastek.com>
2022-08-26 15:28:50 +0530

    "ENC selection page spec file created"

---
+++ Tests/FunctionalTests/ess-ui-encselectionpage.spec.ts
@@ -0,0 +1,26 @@
+import {test, expect, Page, chromium} from '@playwright/test';
+import {esslandingpageObjectsConfig} from '../../PageObjects/essui-landingpageObjects.json';
+import {fssHomePageObjectsConfig} from '../../PageObjects/fss-homepageObjects.json';
+import {uploadFile}from '../../Helper/ESSLandingPageHelper';
+import {autoTestConfig} from '../../appSetting.json';
+import {LoginPortal} from '../../Helper/CommonHelper';
+import {commonObjectsConfig} from '../../PageObjects/commonObjects.json';
+
+test.describe('ESS UI Landing Page Functional Test Scenarios', ()=>{
+
+     test.beforeEach(async({page})=>{
+     
+        await page.goto(autoTestConfig.url);
+        await page.waitForLoadState('load');
+        await LoginPortal(page,autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
+        await page.locator(fssHomePageObjectsConfig.essLinkSelector).click();
+     })
+
+
+    // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13806 
+    test('', async({page})=>{
+            
+    })
+
+
+})
