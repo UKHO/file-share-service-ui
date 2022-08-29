f4780f7f97b0a9631dcee16f0ae8faf6df63c703
Kishor Ambare <Kishor13892@Mastek.Com>
2022-08-29 12:02:02 +0530

    Added function in helper file

--- Helper/ESSLandingPageHelper.ts
+++ Helper/ESSLandingPageHelper.ts
@@ -1,5 +1,5 @@
-import {Page} from '@playwright/test';
-
+import { Page } from '@playwright/test';
+import { esslandingpageObjectsConfig } from '../PageObjects/essui-landingpageObjects.json';
 
 //<summary>
 // Uploading .csv & .txt files
@@ -7,13 +7,23 @@ import {Page} from '@playwright/test';
 //<param> page Object </param>
 //<param> Element Locator </param>
 //<param> File path </param>
-
 export async function uploadFile(page: Page, elementSelector: string, filePath: string) {
 
-const [fileChooserDataFile] = await Promise.all([
-  page.waitForEvent('filechooser'),
-  page.locator(elementSelector).click(),
+  const [fileChooserDataFile] = await Promise.all([
+    page.waitForEvent('filechooser'),
+    page.locator(elementSelector).click(),
   ]);
-await fileChooserDataFile.setFiles(filePath);
+  await fileChooserDataFile.setFiles(filePath);
+}
+
+//<summary>
+// Adding single ENcs
+//</summary>
+//<param> page Object </param>
+//<param> Element Locator </param>
+export async function addSingleENC(page: Page, elementSelector: string) {
+  await page.click(esslandingpageObjectsConfig.addencradiobtnSelector);
+  await page.fill(elementSelector, esslandingpageObjectsConfig.ENCValue2);
+  await page.click(esslandingpageObjectsConfig.proceedButtonSelector);
 
 }
--- Tests/FunctionalTests/ess-ui-encselectionpage.spec.ts
+++ Tests/FunctionalTests/ess-ui-encselectionpage.spec.ts
@@ -4,6 +4,7 @@ import {fssHomePageObjectsConfig} from '../../PageObjects/fss-homepageObjects.js
 import {encselectionpageObjectsConfig} from '../../PageObjects/essui-encselectionpageObjects.json';
 import {autoTestConfig} from '../../appSetting.json';
 import {LoginPortal} from '../../Helper/CommonHelper';
+import {addSingleENC} from '../../Helper/ESSLandingPageHelper';
 import {commonObjectsConfig} from '../../PageObjects/commonObjects.json';
 
 test.describe('ESS UI Landing Page Functional Test Scenarios', ()=>{
@@ -20,15 +21,19 @@ test.describe('ESS UI Landing Page Functional Test Scenarios', ()=>{
      // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13944 (For valid ENC no.)
      // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13945 (For "Your selection" table)
      // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13946 (For "Start again" link)
-     test('Verify that user is able to add a valid single ENCs and link "Start Again" redirects to ESS landing page', async({page})=>{
-      await page.click(esslandingpageObjectsConfig.addencradiobtnSelector);
-      await page.fill(esslandingpageObjectsConfig.addSingleENCTextboxSelector, esslandingpageObjectsConfig.ENCValue2);
-      await page.click(esslandingpageObjectsConfig.proceedButtonSelector); 
+     test.only('Verify that user is able to add a valid single ENCs and link "Start Again" redirects to ESS landing page', async({page})=>{
+      await addSingleENC(page, esslandingpageObjectsConfig.addSingleENCTextboxSelector);
       expect(await page.innerText(encselectionpageObjectsConfig.firstEncSelector)).toEqual(esslandingpageObjectsConfig.ENCValue2);
       await expect(page.locator(esslandingpageObjectsConfig.selectionTextSelector)).toBeVisible();
       await page.click(esslandingpageObjectsConfig.startAgainLinkSelector);
       await expect(page.locator(esslandingpageObjectsConfig.exchangesettextSelector)).toBeVisible();
  })
+
+ // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13957
+ test('Verify that user is not able to add more than Maxlimit ENCs using manually adding ENcs', async({page})=>{
+   await addSingleENC(page, esslandingpageObjectsConfig.addSingleENCTextboxSelector);
+   await page.locator(encselectionpageObjectsConfig.addAnotherENCSelector).click();
+})
 
 
 })
