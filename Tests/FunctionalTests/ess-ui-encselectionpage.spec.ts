7c994167b930c22b794f5499c1dfd8a0b6436033
Sheetal Dalavi <sheetal15692@mastek.com>
2022-08-29 17:27:55 +0530

    optimize code

--- Tests/FunctionalTests/ess-ui-encselectionpage.spec.ts
+++ Tests/FunctionalTests/ess-ui-encselectionpage.spec.ts
@@ -18,11 +18,10 @@ test.describe('ESS UI Landing Page Functional Test Scenarios', ()=>{
         await page.locator(fssHomePageObjectsConfig.essLinkSelector).click();
      })
 
-
      // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13944 (For valid ENC no.)
      // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13945 (For "Your selection" table)
      // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13946 (For "Start again" link)
-     test.only('Verify that user is able to add a valid single ENCs and link "Start Again" redirects to ESS landing page', async({page})=>{
+     test ('Verify that user is able to add a valid single ENCs and link "Start Again" redirects to ESS landing page', async({page})=>{
       await addSingleENC(page, esslandingpageObjectsConfig.addSingleENCTextboxSelector);
       expect(await page.innerText(encselectionpageObjectsConfig.firstEncSelector)).toEqual(esslandingpageObjectsConfig.ENCValue2);
       await expect(page.locator(esslandingpageObjectsConfig.selectionTextSelector)).toBeVisible();
@@ -39,26 +38,18 @@ test.describe('ESS UI Landing Page Functional Test Scenarios', ()=>{
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13954 - Add Anther ENC
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13956 - Duplicate ENC
      test('Verify that after clicking on "Add another ENC" link user able to add another ENC number', async({page})=>{
+       
+       // Add Another ENC link should visible
        await addSingleENC(page, esslandingpageObjectsConfig.addSingleENCTextboxSelector);
-       await page.locator(encselectionpageObjectsConfig.addAnotherENCSelector).click(); 
        await expect (page.locator(encselectionpageObjectsConfig.addAnotherENCSelector)).toBeVisible();
-
-       await page.locator(encselectionpageObjectsConfig.typeENCCellNameHereSelector).fill(encselectionpageObjectsConfig.ENCValue1);
-       await page.locator(esslandingpageObjectsConfig.addsingleencSelector).click(); 
-       await expect (page.locator(encselectionpageObjectsConfig.addAnotherENC2Selector)).toBeVisible();
-      
-       //Add another ENC2
        await addAnotherENC(page, encselectionpageObjectsConfig.addAnotherENCSelector);
-       await expect (page.locator(encselectionpageObjectsConfig.addAnotherENCSelector)).toBeVisible();
-       await expect (page.locator(encselectionpageObjectsConfig.addAnotherENC3Selector)).toBeVisible();
+
+       //Add another ENC2 & Choose box should empty
        expect (await page.isChecked(encselectionpageObjectsConfig.chooseBoxSelecetor)).toBeFalsy();
 
        //13956 - Add another ENC2 - Duplicate No.
        await addAnotherENC(page, encselectionpageObjectsConfig.addAnotherENCSelector);
-       await expect (page.locator(encselectionpageObjectsConfig.addAnotherENCSelector)).toBeVisible();
-       await expect (page.locator(encselectionpageObjectsConfig.addAnotherENC3Selector)).toBeVisible();
-       await expect (page.locator(encselectionpageObjectsConfig.errorMessageForDplicateNumberSelector)).toContainText(encselectionpageObjectsConfig.errorMessage)
+       await expect (page.locator(encselectionpageObjectsConfig.errorMessageForDuplicateNumberSelector)).toContainText(encselectionpageObjectsConfig.errorMessage)
   })
-
 
 })
