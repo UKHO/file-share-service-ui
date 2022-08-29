e6a674dc1321d80b31fe6b3e11112f86aabb1b78
Shivam Verma <shivam15622@mastek.com>
2022-08-29 14:49:15 +0530

    ESS UI Home spec code pushing

--- Tests/FunctionalTests/ess-ui-home.spec.ts
+++ Tests/FunctionalTests/ess-ui-home.spec.ts
@@ -1,5 +1,5 @@
-import {test,expect} from '@playwright/test';
-import { AcceptCookies,LoginPortal } from '../../Helper/CommonHelper';
+import { test, expect } from '@playwright/test';
+import { AcceptCookies, LoginPortal } from '../../Helper/CommonHelper';
 import { autoTestConfig } from '../../appSetting.json';
 import { commonObjectsConfig } from '../../PageObjects/commonObjects.json';
 import { fssSearchPageObjectsConfig } from '../../PageObjects/fss-searchpageObjects.json';
@@ -7,71 +7,87 @@ import { fssHomePageObjectsConfig } from '../../PageObjects/fss-homepageObjects.
 import { esslandingpageObjectsConfig } from '../../PageObjects/essui-landingpageObjects.json';
 
 
-test.describe('ESS UI Home Page Functional Test Scenarios', ()=>{
+test.describe('ESS UI Home Page Functional Test Scenarios', () => {
 
-    test.beforeEach(async({page})=>{
+    test.beforeEach(async ({ page }) => {
 
         await page.goto(autoTestConfig.url);
-        await page.waitForLoadState(); 
-        await AcceptCookies(page);   
-        })
+        await page.waitForLoadState();
+        await AcceptCookies(page);
+    })
 
     test('Verify "Search" link is not available on home page before login', async ({ page }) => {
-        expect (await page.isHidden (fssHomePageObjectsConfig.searchLinkSelector)).toBeTruthy();
-      
+        expect(await page.isHidden(fssHomePageObjectsConfig.searchLinkSelector)).toBeTruthy();
+
     });
 
     test('Verify "Exchange sets" link is not available on home page before login', async ({ page }) => {
-        expect (await page.isHidden (fssHomePageObjectsConfig.essLinkSelector)).toBeTruthy();          
+        expect(await page.isHidden(fssHomePageObjectsConfig.essLinkSelector)).toBeTruthy();
     });
 
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13803
     test('Verify clicking Search link from home page after login, navigates to Search page', async ({ page }) => {
-        await LoginPortal(page,autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
+        await LoginPortal(page, autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
         await page.locator(fssHomePageObjectsConfig.searchLinkSelector).click();
         await page.waitForLoadState('load');
         await expect(page).toHaveTitle("Admiralty - File Share Service - Search");
-        expect(await page.innerHTML(fssSearchPageObjectsConfig.searchPageContainerHeaderSelector)).toEqual(fssSearchPageObjectsConfig.searchPageContainerHeaderText);      
+        expect(await page.innerHTML(fssSearchPageObjectsConfig.searchPageContainerHeaderSelector)).toEqual(fssSearchPageObjectsConfig.searchPageContainerHeaderText);
     });
 
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13796
     test('Verify clicking Exchange sets link after login navigates to ESS landing page', async ({ page }) => {
-        await LoginPortal(page,autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
+        await LoginPortal(page, autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
         await page.locator(fssHomePageObjectsConfig.essLinkSelector).click();
         await page.waitForTimeout(500);
         await expect(page.url()).toContain("/exchangesets");
-        await expect(page).toHaveTitle("Admiralty - File Share Service - Exchange Sets");        
+        await expect(page).toHaveTitle("Admiralty - File Share Service - Exchange Sets");
     });
 
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13803
     test('Verify appending "/search" in  url after login, navigates to Search page', async ({ page }) => {
-        await LoginPortal(page,autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
-        await page.goto(autoTestConfig.url+"/#/search");
+        await LoginPortal(page, autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
+        await page.goto(autoTestConfig.url + "/#/search");
         await page.waitForLoadState('load');
-        expect(await page.innerHTML(fssSearchPageObjectsConfig.searchPageContainerHeaderSelector)).toEqual(fssSearchPageObjectsConfig.searchPageContainerHeaderText); 
-      
+        expect(await page.innerHTML(fssSearchPageObjectsConfig.searchPageContainerHeaderSelector)).toEqual(fssSearchPageObjectsConfig.searchPageContainerHeaderText);
+
     });
 
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13796
-    test.only('Verify appending "/exchangesets" in  url after login, navigates to ESS landing page', async ({ page }) => {
-        await LoginPortal(page,autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
-        await page.goto(autoTestConfig.url+"/#/exchangesets");
+    test('Verify appending "/exchangesets" in  url after login, navigates to ESS landing page', async ({ page }) => {
+        await LoginPortal(page, autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
+        await page.goto(autoTestConfig.url + "/#/exchangesets");
         await page.waitForLoadState('load');
         expect(await page.innerText(esslandingpageObjectsConfig.radioButtonNameSelector)).toEqual(esslandingpageObjectsConfig.radioButton1Name);
     });
 
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13795
     test('Verify appending "/exchangesets" in url before login, navigates to home page', async ({ page }) => {
-        await page.goto(autoTestConfig.url+"/#/exchangesets");
-        await expect(page).toHaveTitle("Admiralty - File Share Service");    
-        expect (await page.isHidden (fssHomePageObjectsConfig.essLinkSelector)).toBeTruthy();   
+        await page.goto(autoTestConfig.url + "/#/exchangesets");
+        await expect(page).toHaveTitle("Admiralty - File Share Service");
+        expect(await page.isHidden(fssHomePageObjectsConfig.essLinkSelector)).toBeTruthy();
     });
 
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13797   
     test('Verify appending "/search" in url before login, navigates to home page', async ({ page }) => {
-        await page.goto(autoTestConfig.url+"/#/search");
-        await expect(page).toHaveTitle("Admiralty - File Share Service");    
-        expect (await page.isHidden (fssHomePageObjectsConfig.searchLinkSelector)).toBeTruthy();   
+        await page.goto(autoTestConfig.url + "/#/search");
+        await expect(page).toHaveTitle("Admiralty - File Share Service");
+        expect(await page.isHidden(fssHomePageObjectsConfig.searchLinkSelector)).toBeTruthy();
+    });
+
+    // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13964
+    test('Verfiying Exchange Set Text and Link', async ({ page }) => {
+        await LoginPortal(page, autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
+        await expect(page.locator(fssHomePageObjectsConfig.exchngSetTextSelector)).toHaveText('Exchange sets');
+        await expect(page.locator(fssHomePageObjectsConfig.exchngSetLinkSelector)).toHaveText('Make an exchange set');
+    });
+
+    //https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13964
+    test('Verifying "Make an exchange set" link should redirect to exchangeset landing page', async ({ page }) => {
+        await LoginPortal(page, autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
+        await page.locator(fssHomePageObjectsConfig.exchngSetLinkSelector).click();
+        await page.waitForLoadState();
+        await expect(page.innerHTML(fssHomePageObjectsConfig.exchngSetHdngSelector)).toEqual('Exchange sets');;
+        await expect(page.url()).toContain("/exchangesets");
     });
 
 });
