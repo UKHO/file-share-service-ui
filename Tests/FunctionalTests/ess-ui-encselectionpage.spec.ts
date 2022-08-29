227001d58d67dd538c0f5ddf31ce2275f95353af
Kishor Ambare <Kishor13892@Mastek.Com>
2022-08-29 11:25:49 +0530

    Added page object file

--- Tests/FunctionalTests/ess-ui-encselectionpage.spec.ts
+++ Tests/FunctionalTests/ess-ui-encselectionpage.spec.ts
@@ -1,7 +1,7 @@
 import {test, expect, Page, chromium} from '@playwright/test';
 import {esslandingpageObjectsConfig} from '../../PageObjects/essui-landingpageObjects.json';
 import {fssHomePageObjectsConfig} from '../../PageObjects/fss-homepageObjects.json';
-import {uploadFile}from '../../Helper/ESSLandingPageHelper';
+import {encselectionpageObjectsConfig} from '../../PageObjects/essui-encselectionpageObjects.json';
 import {autoTestConfig} from '../../appSetting.json';
 import {LoginPortal} from '../../Helper/CommonHelper';
 import {commonObjectsConfig} from '../../PageObjects/commonObjects.json';
@@ -17,10 +17,18 @@ test.describe('ESS UI Landing Page Functional Test Scenarios', ()=>{
      })
 
 
-    // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13806 
-    test('', async({page})=>{
-            
-    })
+     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13944 (For valid ENC no.)
+     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13945 (For "Your selection" table)
+     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/13946 (For "Start again" link)
+     test('Verify that user is able to add a valid single ENCs and link "Start Again" redirects to ESS landing page', async({page})=>{
+      await page.click(esslandingpageObjectsConfig.addencradiobtnSelector);
+      await page.fill(esslandingpageObjectsConfig.addSingleENCTextboxSelector, esslandingpageObjectsConfig.ENCValue2);
+      await page.click(esslandingpageObjectsConfig.proceedButtonSelector); 
+      expect(await page.innerText(encselectionpageObjectsConfig.firstEncSelector)).toEqual(esslandingpageObjectsConfig.ENCValue2);
+      await expect(page.locator(esslandingpageObjectsConfig.selectionTextSelector)).toBeVisible();
+      await page.click(esslandingpageObjectsConfig.startAgainLinkSelector);
+      await expect(page.locator(esslandingpageObjectsConfig.exchangesettextSelector)).toBeVisible();
+ })
 
 
 })
