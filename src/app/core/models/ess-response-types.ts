4408b527a102755714db4e7399757c5d99a6e7b1
Pratibha Shinde <Pratibh14837@Mastek.Com>
2022-08-29 12:14:16 +0530

    AB#13028 - removed unwanted space

--- src/app/core/models/ess-response-types.ts
+++ src/app/core/models/ess-response-types.ts
@@ -1,37 +1,19 @@
 export interface ExchangeSetDetails {
-
     links: ExchangeSetLinks;
-
     urlExpiryDateTime: Date;
-
     requestedProductCount: number;
-
     exchangeSetCellCount: number;
-
     requestedProductsAlreadyUpToDateCount: number;
-
     requestedProductsNotInExchangeSet: ProductsNotInExchangeSet[]
-
 }
-
-
 
 export interface ExchangeSetLinks {
-
     batchStatusUri: string;
-
     batchDetailsUri: string;
-
     fileUri: string;
-
 }
 
-
-
 export interface ProductsNotInExchangeSet {
-
     productName: string;
-
     reason: string;
-
 }
