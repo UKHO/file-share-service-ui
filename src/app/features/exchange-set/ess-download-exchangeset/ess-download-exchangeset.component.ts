f811be5d21b01350b81d20ef82f5424f9f5c9255
Pratibha Shinde <Pratibh14837@Mastek.Com>
2022-08-30 17:47:57 +0530

    AB#13208-code chages for display the requestedProductsNotInExchangeSet

--- src/app/features/exchange-set/ess-download-exchangeset/ess-download-exchangeset.component.ts
+++ src/app/features/exchange-set/ess-download-exchangeset/ess-download-exchangeset.component.ts
@@ -10,14 +10,14 @@ import { ExchangeSetDetails } from 'src/app/core/models/ess-response-types';
 export class EssDownloadExchangesetComponent implements OnInit {
  
   result:ExchangeSetDetails;
-
+  requestedProductsNotInExchangeSet : any[];
+  
   constructor(private essUploadFileService: EssUploadFileService) { 
-
   }
 
   ngOnInit(): void {
     this.result=this.essUploadFileService.getExchangeSetDetails();
-
-  }
+    this.requestedProductsNotInExchangeSet = this.result.requestedProductsNotInExchangeSet;
+ } 
 
 }
