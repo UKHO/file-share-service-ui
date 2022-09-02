d60e927dbe9671d2b9b3875b29ecfcaabb26c9a1
Shital Kadam <shital15683@mastek.com>
2022-09-02 15:26:18 +0530

    added Microsoft edge browser in config file.

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,12 +34,12 @@ const config: PlaywrightTestConfig = {
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-      },
-    },
+    //{
+      //name: 'chromium',
+      //use: {
+        //...devices['Desktop Chrome'],
+      //},
+    //},
 //// Note: Flaky Tests in pipeline, so commenting below browsers for now. Discussed & agreed with Ravi.
     // {
     //   name: 'firefox',
@@ -55,12 +55,12 @@ const config: PlaywrightTestConfig = {
     //   },
     // },
 
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge'
-    //   }
-    // }
+    {
+      name: 'Microsoft Edge',
+     use: {
+        channel: 'msedge'
+       }
+    }
    
   ],
 
