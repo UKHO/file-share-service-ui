cf76ff9e66a2744a69b79f5e57e3a638bcb5f594
Rashi Mishra <rashi14748@mastek.com>
2022-09-16 18:37:28 +0530

    14141- refactored ess ui functional tests AB#14141

---
+++ PageObjects/essui-esdownloadpageObjects.ts
@@ -0,0 +1,100 @@
+import { Page, Locator, expect } from "@playwright/test";
+
+
+export class EsDownloadPageObjects {
+    
+    readonly expect: EsDownloadPageAssertions;
+    readonly selectAllSelector: Locator;
+    readonly requestENCsSelector: Locator;
+    readonly downloadTextSelector: Locator;
+    readonly downloadButtonSelector: Locator;
+    readonly spinnerSelector: Locator;
+    readonly includedENCsCountSelector: Locator;
+    readonly EstimatedESsizeSelector: Locator;
+    readonly selectedTextSelector: Locator;
+    readonly countInvalidENCsSelector: Locator;
+    readonly invalidEncsSelector: Locator;
+    readonly errorMessageSelector: Locator;
+
+    constructor(readonly page: Page) {
+        this.expect = new EsDownloadPageAssertions(this);
+        this.selectAllSelector = this.page.locator("//a[text()='Select all']");
+        this.requestENCsSelector = this.page.locator("ukho-button.requestEncBtn");
+        this.downloadTextSelector = this.page.locator("//h3[text()='Exchange set download']");
+        this.downloadButtonSelector = this.page.locator("ukho-button.btnDownload");
+        this.spinnerSelector = this.page.locator("i.fas.fa-circle-notch.fa-spin");
+        this.includedENCsCountSelector = this.page.locator("//strong[@class='f21'][2]");
+        this.EstimatedESsizeSelector = this.page.locator("//p[@class='f21']");
+        this.selectedTextSelector = this.page.locator("div[id='contentArea'] strong:nth-child(1)");
+        this.countInvalidENCsSelector = this.page.locator("div[class='exchangesetcontainer'] li");
+        this.invalidEncsSelector = this.page.locator("div[id='contentArea'] li");
+        this.errorMessageSelector = this.page.locator("text = There has been an error");
+    }
+    
+    async selectAllSelectorClick(): Promise<void> {
+        await this.selectAllSelector.click();
+    }
+
+    async requestENCsSelectorClick(): Promise<void> {
+        await this.requestENCsSelector.click();
+    }
+}
+
+class EsDownloadPageAssertions {
+    constructor(readonly esDownloadPageObjects: EsDownloadPageObjects) {
+    }
+
+    async spinnerSelectorVisible(): Promise<void> {
+        
+        expect(await this.esDownloadPageObjects.spinnerSelector.isVisible).toBeTruthy();
+    }
+    
+    async spinnerSelectorHidden(): Promise<void> {
+        
+        expect(await this.esDownloadPageObjects.spinnerSelector.isVisible).toBeFalsy();
+    }
+
+    async downloadButtonSelectorEnabled(): Promise<void> {
+       
+        expect(await this.esDownloadPageObjects.downloadButtonSelector.isVisible).toBeTruthy();
+    }
+
+    async selectedTextSelectorVisible(): Promise<void> {
+        
+        expect(await this.esDownloadPageObjects.selectedTextSelector).toBeTruthy();
+    }
+
+    async includedENCsCountSelectorVisible(): Promise<void> {
+
+        expect(await this.esDownloadPageObjects.includedENCsCountSelector).toBeTruthy();
+    }
+
+    async errorMessageSelectorDisplayed(): Promise<void> {
+        
+        expect(await this.esDownloadPageObjects.errorMessageSelector.innerText()).toBeTruthy();
+    }
+    
+    async VerifyExchangeSetSize(): Promise<void> {
+
+        let ENCsIncluded = parseInt(((await this.esDownloadPageObjects.includedENCsCountSelector.innerHTML()).split(' '))[0]);
+
+        if (ENCsIncluded < 4) {
+             expect(await this.esDownloadPageObjects.EstimatedESsizeSelector.innerText).toEqual('Estimated size ' + Math.round(ENCsIncluded * (0.3) * 1024) + 'KB');
+        }
+        else {
+             expect(await this.esDownloadPageObjects.EstimatedESsizeSelector.innerText).toEqual('Estimated size ' + ENCsIncluded * (0.3) + 'MB');
+        }
+
+    }
+
+    async ValidateInvalidENCsAsPerCount(): Promise<void> {
+
+        let invalidEncsCount = await this.esDownloadPageObjects.countInvalidENCsSelector.count();
+        let inValidEncs = await this.esDownloadPageObjects.invalidEncsSelector;
+
+        for (var i = 1; i <= invalidEncsCount; i++) {
+            
+           expect(await inValidEncs.nth(i).innerText).toBeTruthy();
+        }
+    }
+}
--- Tests/FunctionalTests/ess-ui-esdownloadpage.spec.ts
+++ Tests/FunctionalTests/ess-ui-esdownloadpage.spec.ts
@@ -1,170 +1,106 @@
-import { test, expect } from '@playwright/test';
-import { esslandingpageObjectsConfig } from '../../PageObjects/essui-landingpageObjects.json';
-import { fssHomePageObjectsConfig } from '../../PageObjects/fss-homepageObjects.json';
-import { uploadFile, } from '../../Helper/ESSLandingPageHelper';
+import { test } from '@playwright/test';
 import { autoTestConfig } from '../../appSetting.json';
 import { LoginPortal } from '../../Helper/CommonHelper';
+import { fssHomePageObjectsConfig } from '../../PageObjects/fss-homepageObjects.json';
 import { commonObjectsConfig } from '../../PageObjects/commonObjects.json';
-import { encselectionpageObjectsConfig } from '../../PageObjects/essui-encselectionpageObjects.json';
-import { downloadpageObjectsConfig } from '../../PageObjects/essui-esdownloadpageObjects.json'
+import { EssLandingPageObjects } from '../../PageObjects/essui-landingpageObjects';
+import { EncSelectionPageObjects } from '../../PageObjects/essui-encselectionpageObjects';
+import { EsDownloadPageObjects } from '../../PageObjects/essui-esdownloadpageObjects';
+import { apiRoute400, apiRoute403, apiRoute500, apiRoute200, apiRoute200WithExcludedENCs } from './ess-api-mock';
 
 test.describe('ESS UI ES Download Page Functional Test Scenarios', () => {
 
+    let esslandingPageObjects: EssLandingPageObjects;
+    let encSelectionPageObjects: EncSelectionPageObjects;
+    let esDownloadPageObjects: EsDownloadPageObjects;
+
     test.beforeEach(async ({ page }) => {
+
+        esslandingPageObjects = new EssLandingPageObjects(page);
+        encSelectionPageObjects = new EncSelectionPageObjects(page);
+        esDownloadPageObjects = new EsDownloadPageObjects(page);
+
         await page.goto(autoTestConfig.url);
         await page.waitForLoadState('load');
         await LoginPortal(page, autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
         await page.locator(fssHomePageObjectsConfig.essLinkSelector).click();
-        await page.click(esslandingpageObjectsConfig.uploadradiobtnSelector);
-        await uploadFile(page, esslandingpageObjectsConfig.chooseuploadfileSelector, './Tests/TestData/downloadValidAndInvalidENCs.csv');
-        await page.click(esslandingpageObjectsConfig.proceedButtonSelector);
-        await page.click(downloadpageObjectsConfig.selectAllSelector);
+        await esslandingPageObjects.uploadradiobtnSelectorClick();
+        await esslandingPageObjects.uploadFile(page, './Tests/TestData/test.csv');
+        await esslandingPageObjects.proceedButtonSelectorClick();
+        await esDownloadPageObjects.selectAllSelectorClick();
     })
 
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/14092
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/14093
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/14094
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/14095
-    test('Verify Estimated Size of ES & Spinner untill Download button appears', async ({ page }) => {
-        await page.click(downloadpageObjectsConfig.requestENCsSelector);
-        await expect(page.locator(downloadpageObjectsConfig.spinnerSelector)).toBeVisible();
-        await page.waitForSelector(downloadpageObjectsConfig.downloadButtonSelector);
-        await expect(page.locator(downloadpageObjectsConfig.spinnerSelector)).toBeHidden();
-        await expect(page.locator(downloadpageObjectsConfig.downloadButtonSelector)).toBeVisible();
+    test.only('Verify Estimated Size of ES & Spinner untill Download button appears', async ({ page }) => {
+       
+        await esDownloadPageObjects.requestENCsSelectorClick();
+        await page.waitForTimeout(1000);
 
-        let ENCsIncluded = parseInt(((await page.innerHTML(downloadpageObjectsConfig.includedENCsCountSelector)).split(' '))[0]);
-        if (ENCsIncluded < 4) {
-            await expect(page.locator(downloadpageObjectsConfig.EstimatedESsizeSelector)).toContainText('Estimated size ' + Math.round(ENCsIncluded * (0.3) * 1024) + 'KB');
-        }
-        else {
-            await expect(page.locator(downloadpageObjectsConfig.EstimatedESsizeSelector)).toContainText('Estimated size ' + ENCsIncluded * (0.3) + 'MB');
-        }
-
+        // await esDownloadPageObjects.expect.spinnerSelectorVisible();
+        // await esDownloadPageObjects.downloadTextSelector.waitFor({state: 'visible'});
+        // await esDownloadPageObjects.expect.spinnerSelectorHidden();
+       
+        await esDownloadPageObjects.expect.downloadButtonSelectorEnabled();
+        await esDownloadPageObjects.expect.VerifyExchangeSetSize();
     })
 
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/14101 (Response 400 --> NOT 200 - Getting Error Message)
-    test ('400 scenario', async ({ page }) => {
-        page.route('**/productData/productIdentifiers', async (route, request) => {
-            await route.fulfill({
-                status: 400,
-                body: 'Bad Request - Mock Response edited'
-            });
-        });
-        await page.click(downloadpageObjectsConfig.requestENCsSelector)
-        expect(await page.isVisible(downloadpageObjectsConfig.errorMessageSelector)).toBeTruthy();
+    test('400 scenario', async ({ page }) => {
 
+        await apiRoute400(page);
+        await esDownloadPageObjects.requestENCsSelectorClick();
+
+        await esDownloadPageObjects.expect.errorMessageSelectorDisplayed();
     });
 
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/14101 (Response 403 --> NOT 200 - Getting  Error Message)
-    test ('403 scenario', async ({ page }) => {
-        page.route('**/productData/productIdentifiers', async (route, request) => {
-            await route.fulfill({
-                status: 403,
-                body: 'Forbidden - Mock Response edited'
-            });
-        });
-        await page.click(downloadpageObjectsConfig.requestENCsSelector)
-        expect(await page.isVisible(downloadpageObjectsConfig.errorMessageSelector)).toBeTruthy();
+    test('403 scenario', async ({ page }) => {
+
+        await apiRoute403(page);
+        await esDownloadPageObjects.requestENCsSelectorClick();
+
+        await esDownloadPageObjects.expect.errorMessageSelectorDisplayed();
     });
 
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/14101 (Response 500 --> NOT 200 - Getting Error Message)
-    test ('500 scenario', async ({ page }) => {
-        page.route('**/productData/productIdentifiers', async (route, request) => {
-            await route.fulfill({
-                status: 500,
-                body: 'Internal server Error - Mock Response edited'
-            });
-        });
-        await page.click(downloadpageObjectsConfig.requestENCsSelector)
-        expect(await page.isVisible(downloadpageObjectsConfig.errorMessageSelector)).toBeTruthy();
+    test('500 scenario', async ({ page }) => {
+
+        await apiRoute500(page);
+        await esDownloadPageObjects.requestENCsSelectorClick();
+
+        await esDownloadPageObjects.expect.errorMessageSelectorDisplayed();
     });
 
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/14129 (200 - OK)
-    test ('200 Scenario', async ({ page }) => {
-        await page.click(encselectionpageObjectsConfig.startAgainLinkSelector);
-        await page.click(esslandingpageObjectsConfig.uploadradiobtnSelector);
-        await uploadFile(page, esslandingpageObjectsConfig.chooseuploadfileSelector, './Tests/TestData/downloadValidENCs.csv');
-        await page.click(esslandingpageObjectsConfig.proceedButtonSelector);
-        await page.click(downloadpageObjectsConfig.selectAllSelector);
-        
-        page.route('**/productData/productIdentifiers', async (route, request) => {
-            await route.fulfill({
-                status: 200,
-                contentType: 'application/json',
-                body: JSON.stringify({
-                    "_links": {
-                        "exchangeSetBatchStatusUri": {
-                            "href": "https://uatadmiralty.azure-api.net/fss-qa/batch/9a0e19b3-58a9-4768-9acb-d25781a6923c/status"
-                        },
-                        "exchangeSetBatchDetailsUri": {
-                            "href": "https://uatadmiralty.azure-api.net/fss-qa/batch/9a0e19b3-58a9-4768-9acb-d25781a6923c"
-                        },
-                        "exchangeSetFileUri": {
-                            "href": "https://uatadmiralty.azure-api.net/fss-qa/batch/9a0e19b3-58a9-4768-9acb-d25781a6923c/files/V01X01.zip"
-                        }
-                    },
-                    "exchangeSetUrlExpiryDateTime": "2022-09-15T11:42:26.271Z",
-                    "requestedProductCount": 13,
-                    "exchangeSetCellCount": 13,
-                    "requestedProductsAlreadyUpToDateCount": 0,
-                    "requestedProductsNotInExchangeSet": []
-                })
-            });
-        })
-        await page.click(downloadpageObjectsConfig.requestENCsSelector);
-        await page.waitForSelector(downloadpageObjectsConfig.downloadButtonSelector);
-        expect(await page.isVisible(downloadpageObjectsConfig.downloadButtonSelector)).toBeTruthy();
-        expect(await page.isVisible(downloadpageObjectsConfig.selectedTextSelector)).toBeTruthy(); // select
-        expect(await page.isVisible(downloadpageObjectsConfig.includedENCsCountSelector)).toBeTruthy(); // include
+    test('200 Scenario', async ({ page }) => {
+
+        await encSelectionPageObjects.startAgainLinkSelectorClick();
+        await esslandingPageObjects.uploadradiobtnSelectorClick();
+        await esslandingPageObjects.uploadFile(page, './Tests/TestData/downloadValidENCs.csv');
+        await esslandingPageObjects.proceedButtonSelectorClick();
+        await esDownloadPageObjects.selectAllSelectorClick();
+
+        await apiRoute200(page);
+        await esDownloadPageObjects.requestENCsSelectorClick();
+        await page.waitForTimeout(1000);
+
+        await esDownloadPageObjects.expect.downloadButtonSelectorEnabled();
+        await esDownloadPageObjects.expect.selectedTextSelectorVisible();
+        await esDownloadPageObjects.expect.includedENCsCountSelectorVisible();
     });
 
     // https://dev.azure.com/ukhocustomer/File-Share-Service/_workitems/edit/14130 (For 200 but exclude some ENCs)
-    test ('200 Scenario but exclude some ENCs', async ({ page }) => {
-        page.route('**/productData/productIdentifiers', async (route, request) => {
-            await route.fulfill({
-                status: 200,
-                contentType: 'application/json',
-                body: JSON.stringify({
-                    "_links": {
-                        "exchangeSetBatchStatusUri": {
-                            "href": "https://uatadmiralty.azure-api.net/fss-qa/batch/098df5af-7f15-4a78-9734-3d47341a383f/status"
-                        },
-                        "exchangeSetBatchDetailsUri": {
-                            "href": "https://uatadmiralty.azure-api.net/fss-qa/batch/098df5af-7f15-4a78-9734-3d47341a383f"
-                        },
-                        "exchangeSetFileUri": {
-                            "href": "https://uatadmiralty.azure-api.net/fss-qa/batch/098df5af-7f15-4a78-9734-3d47341a383f/files/V01X01.zip"
-                        }
-                    },
-                    "exchangeSetUrlExpiryDateTime": "2022-09-15T11:44:16.659Z",
-                    "requestedProductCount": 18,
-                    "exchangeSetCellCount": 15,
-                    "requestedProductsAlreadyUpToDateCount": 0,
-                    "requestedProductsNotInExchangeSet": [
-                        {
-                            "productName": "AU220150",
-                            "reason": "invalidProduct"
-                        },
-                        {
-                            "productName": "AU5PTL01",
-                            "reason": "invalidProduct"
-                        },
-                        {
-                            "productName": "CN484220",
-                            "reason": "invalidProduct"
-                        }
-                    ]
-                })
-            });
-        })
-        await page.click(downloadpageObjectsConfig.requestENCsSelector);
-        await page.waitForSelector(downloadpageObjectsConfig.downloadButtonSelector);
-        expect(await page.isVisible(downloadpageObjectsConfig.downloadButtonSelector)).toBeTruthy();
-        let count = await page.locator(downloadpageObjectsConfig.countInvalidENCsSelector).count();
-        for (var i = 1; i <= count; i++) {
-            expect(await page.isVisible("div[id='contentArea'] li:nth-child(" + i + ")")).toBeTruthy();
-        }
-        expect(await page.isVisible(downloadpageObjectsConfig.selectedTextSelector)).toBeTruthy();
-        expect(await page.isVisible(downloadpageObjectsConfig.includedENCsCountSelector)).toBeTruthy();
+    test('200 Scenario but exclude some ENCs', async ({ page }) => {
+
+        await apiRoute200WithExcludedENCs(page);
+        await esDownloadPageObjects.requestENCsSelectorClick();
+        
+        await esDownloadPageObjects.expect.downloadButtonSelectorEnabled();
+        await esDownloadPageObjects.expect.ValidateInvalidENCsAsPerCount();
+        await esDownloadPageObjects.expect.selectedTextSelectorVisible();
+        await esDownloadPageObjects.expect.includedENCsCountSelectorVisible();
     });
 })
