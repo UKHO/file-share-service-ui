9a3172d1efca15fb6ae45781c6c5c0c9a7ad02da
Kishor Ambare <Kishor13892@Mastek.Com>
2022-09-21 12:05:59 +0530

    Deselector locator issue

--- PageObjects/essui-encselectionpageObjects.ts
+++ PageObjects/essui-encselectionpageObjects.ts
@@ -63,7 +63,7 @@ export class EncSelectionPageObjects {
         this.ENCTableCheckboxlist = this.page.locator('(//input[@type="checkbox"])');
         this.ENCSelectedTablelist = this.page.locator("(//table/tbody)[2]/tr/td[1]");        
         this.selectAllSelector = this.page.locator("//a[text()=' Select all ']")
-        this.deselectAllSelector = this.page.locator("//a[text()='Deselect all']")
+        this.deselectAllSelector = this.page.locator("//a[text()=' Deselect all ']")
         this.leftTableMesgSelector = this.page.locator('(//span[@class="showListEncTOtal"])[1]')
         this.rightTableMesgSelector = this.page.locator('(//span[@class="showListEncTOtal"])[2]')
         this.requestENCsSelector = this.page.locator("ukho-button.requestEncBtn");
