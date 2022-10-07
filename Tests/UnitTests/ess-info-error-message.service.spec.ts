1cce83642201013bd783e6087d38d1bb935da034
Suraj Singh <suraj49470@gmail.com>
2022-10-07 17:05:27 +0530

    info error message service done

--- Tests/UnitTests/ess-info-error-message.service.spec.ts
+++ Tests/UnitTests/ess-info-error-message.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { EssInfoErrorMessageService } from '../../src/app/core/services/ess-info-error-message.service';
+import { EssInfoErrorMessageService, InfoErrorMessage } from '../../src/app/core/services/ess-info-error-message.service';
 
 describe('EssInfoErrorMessageService', () => {
   let service: EssInfoErrorMessageService;
@@ -13,4 +13,25 @@ describe('EssInfoErrorMessageService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('infoErrorMessage should not be null' , () =>{
+    expect(service.infoErrMessage).not.toBeNull();
+  });
+
+  it('showInfoMessageSubject should not be null' , () => {
+    expect(service.showInfoMessageBSubject).not.toBeNull();
+  });
+
+  it('showInfoErrorMessage should emit messages from showInfoMessageBSubject' , () => {
+    const errObj: InfoErrorMessage = {
+      showInfoErrorMessage : true,
+      messageType : 'error',
+      messageDesc : 'Error Test Message'
+    };
+    service.showInfoMessageBSubject.next(errObj);
+    service.showInfoMessageBSubject.subscribe((errMsg) => {
+      expect(errMsg).toStrictEqual(errObj);
+    });
+  });
+
 });
