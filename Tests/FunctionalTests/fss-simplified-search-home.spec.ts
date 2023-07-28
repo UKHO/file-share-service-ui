55aeae0c032288e07820cee3c2b1ec6220f5f8fc
Richard Hernandez <richard.hernandez@ukho.gov.uk>
2023-07-28 16:01:41 +0100

    update fss-simplified-search-home

--- Tests/FunctionalTests/fss-simplified-search-home.spec.ts
+++ Tests/FunctionalTests/fss-simplified-search-home.spec.ts
@@ -3,20 +3,21 @@ import { autoTestConfig } from '../../appSetting.json';
 import { commonObjectsConfig } from '../../PageObjects/commonObjects.json';
 import { fssSearchPageObjectsConfig } from '../../PageObjects/fss-searchpageObjects.json';
 import { AcceptCookies, LoginPortal } from '../../Helper/CommonHelper';
+import { ClickWaitRetry } from '../../Helper/SearchPageHelper';
 
 test.describe('Test Search Attribute Scenario On Simplified Search Page', () => {
   
   test.beforeEach(async ({ page }) => {
       await page.goto(autoTestConfig.url)
       await AcceptCookies(page);
-      await LoginPortal(page,autoTestConfig.user, autoTestConfig.password, commonObjectsConfig.loginSignInLinkSelector);
+      await LoginPortal(page,autoTestConfig.user, autoTestConfig.password);
       await page.waitForSelector(fssSearchPageObjectsConfig.searchPageContainerHeaderSelector);
       expect(await page.innerHTML(fssSearchPageObjectsConfig.searchPageContainerHeaderSelector)).toEqual(fssSearchPageObjectsConfig.searchPageContainerHeaderText);
   })
 
   test('Verify user landed on "Simplified Search" page', async ({ page }) => {
        
-    var simplifiedSearchBox= (await page.$$(fssSearchPageObjectsConfig.inputSimplifiedSearchBoxSelector)).length
+    var simplifiedSearchBox = (await page.$$('.simplifiedSection')).length
     expect(simplifiedSearchBox).toEqual(1);
     var advanceSearchElements =(await page.$$("#fss-querytable-field-1 input,fss-querytable-operator-1 select,#fss-querytable-value-1 input")).length
     expect(advanceSearchElements).toEqual(0);
@@ -28,9 +29,8 @@ test.describe('Test Search Attribute Scenario On Simplified Search Page', () => 
   })
 
   test('Does it display "Error message" if user clicks on search button and simplified search box is empty', async ({ page }) => {
-    await page.waitForSelector(fssSearchPageObjectsConfig.inputSimplifiedSearchBoxSelector);
-    await page.click(fssSearchPageObjectsConfig.simplifiedSearchButtonSelector);
-    var errorMessage = await page.innerText(fssSearchPageObjectsConfig.dialogWarningSelector);
+    await ClickWaitRetry(page, fssSearchPageObjectsConfig.searchAttributeButton, fssSearchPageObjectsConfig.dialogInfoSelector);
+    var errorMessage = await page.innerText(fssSearchPageObjectsConfig.dialogTitleSelector);
     expect(errorMessage).toContain(fssSearchPageObjectsConfig.warningMessageValue);
   }) 
 
