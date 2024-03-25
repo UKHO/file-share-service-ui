5bbc8a2a766c98f6d11a3cd857555e63646f0270
satyam15302 <satyam15302@mastek.com>
2024-03-25 16:56:45 +0530

    AB#144902 Added Unit tests.

--- Tests/UnitTests/ess-types.component.spec.ts
+++ Tests/UnitTests/ess-types.component.spec.ts
@@ -1,25 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
+import { NO_ERRORS_SCHEMA, DebugElement, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AppConfigService } from '../../src/app/core/services/app-config.service';
 import { EssTypesComponent } from '../../src/app/features/exchange-set/ess-types/ess-types.component';
+import { EssUploadFileService } from '../../src/app/core/services/ess-upload-file.service';
+import { EssInfoErrorMessageService } from '../../src/app/core/services/ess-info-error-message.service';
+import { EssInfoErrorMessageComponent } from '../../src/app/features/exchange-set/ess-info-error-message/ess-info-error-message.component';
+import { CommonModule } from '@angular/common';
 
 describe('EssTypesComponent', () => {
   let component: EssTypesComponent;
   let fixture: ComponentFixture<EssTypesComponent>;
-  let router: Router;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [EssTypesComponent]
+  let service: EssUploadFileService;
+  let essInfoErrorMessageService: EssInfoErrorMessageService;
+  const router = {
+    navigate: jest.fn()
+  };
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule,CommonModule],
+      declarations: [EssTypesComponent, EssInfoErrorMessageComponent],
+      providers: [EssUploadFileService, EssInfoErrorMessageService,{
+        provide: Router,
+        useValue: router
+      },
+    ],
+    schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
-
-    fixture = TestBed.createComponent(EssTypesComponent);
-    component = fixture.componentInstance;
-    router = TestBed.inject(Router);
-    fixture.detectChanges();
-  });
-
+});
+    beforeEach(() => {
+      AppConfigService.settings = {
+        essConfig: {
+          MaxEncLimit: 10,
+          aioExcludeEncs :["GB800001","FR800001"]
+        }
+      };
+  
+      fixture = TestBed.createComponent(EssTypesComponent);
+      service = TestBed.inject(EssUploadFileService);
+      essInfoErrorMessageService = TestBed.inject(EssInfoErrorMessageService);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  
+  
   it('should create', () => {
     expect(component).toBeTruthy();
   });
@@ -42,19 +67,56 @@ describe('EssTypesComponent', () => {
     expect(routerSpy).toHaveBeenCalledWith(['exchangesets', 'exchange-set']);
   });
 
-it('should navigate to "exchangesets/exchange-set" on Proceed button click when Base option is selected', () => {
-    const navigateSpy = jest.spyOn(router, 'navigate');
+  it('should navigate to "exchangesets/exchange-set" on Proceed button click when Base option is selected', () => {
     component.onOptionChange('base');
     component.onProceedClicked();
+    expect(router.navigate).toHaveBeenCalledWith(['exchangesets', 'exchange-set']);
+  });
+
+  it('should navigate to next page when delta download option is selected and date is valid', () => {
+    const navigateSpy = jest.spyOn(router, 'navigate');
+    const validDate = new Date();
+    validDate.setDate(validDate.getDate() - 10); 
+    component.selectedOption = 'delta'; 
+    component.isDateValid = true; 
+    component.isDateSelected = true;
+    component.onDateChange({ target: { valueAsDate: validDate } } as any); 
+
+    component.onProceedClicked(); 
+
     expect(navigateSpy).toHaveBeenCalledWith(['exchangesets', 'exchange-set']);
   });
 
-  it('should not navigate to "exchangesets/exchange-set" on proceed button click when either radio button or date is not selected', () => {
-    const routerSpy = jest.spyOn(router, 'navigate');
-    component.onOptionChange('delta');
-    const event: any = { target: { valueAsDate: null} };
-    component.onDateChange(event);
-    component.onProceedClicked();
-    expect(routerSpy).not.toHaveBeenCalled();
+  it('should show error message when delta option is selected but date is in the future', () => {
+    const futureDate = new Date();
+    futureDate.setDate(futureDate.getDate() + 1); 
+
+    component.selectedOption = 'delta'; 
+    component.isDateSelected = true; 
+    component.onDateChange({ target: { valueAsDate: futureDate } } as any); 
+
+    const errorMessage = 'Please choose a date from today or up to 27 days in the past. Future dates are not permitted.';
+    const errObj={
+      showInfoErrorMessage: true,
+      messageType: 'info',
+      messageDesc: errorMessage}
+    expect(essInfoErrorMessageService.infoErrMessage).toStrictEqual(errObj);
   });
+  it('should show error message when delta option is selected but date is greater than 27 days in the past', () => {
+    const pastDate = new Date();
+    pastDate.setDate(pastDate.getDate() - 28); 
+
+    component.selectedOption = 'delta'; 
+    component.isDateSelected = true; 
+    component.onDateChange({ target: { valueAsDate: pastDate } } as any); 
+
+    const errorMessage = "Please select Base Download for duration greater than 27 days from today's date.";
+    const errObj = {
+      showInfoErrorMessage: true,
+      messageType: 'info',
+      messageDesc: errorMessage
+    };
+    expect(essInfoErrorMessageService.infoErrMessage).toStrictEqual(errObj);
+});
+
 });
