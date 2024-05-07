15719d00e512f89e23709589217c76409c5f5332
Anuj P <anuj.patil@mastek.com>
2024-05-07 11:49:46 +0530

    AB#151567 added UTs

--- Tests/UnitTests/ess-list-encs.component.spec.ts
+++ Tests/UnitTests/ess-list-encs.component.spec.ts
@@ -698,6 +698,24 @@ describe('EssListEncsComponent', () => {
     tick();
     expect(component.displayLoader).toEqual(true);
   }));
+
+  it('should have "S63 Exchange Set" selected by default',()=>{
+    expect(component.selectedOption).toEqual('S63');
+  });
+
+  it('when "Request ENCs" button is clicked, at least one radio button must be selected',()=>{
+    let requestENCs = fixture.debugElement.nativeElement.querySelector('admiralty-button');
+    requestENCs.click();
+
+    expect(component.selectedOption).toBeTruthy();
+  });
+
+  it('should not display radio buttons when user is not privileged', () => {
+    component.isPrivilegedUser = false;
+    fixture.detectChanges();
+    const radioButtons = fixture.debugElement.queryAll(By.css('admiralty-radio'));
+    expect(radioButtons.length).toBe(0);
+  });
 });
 
 export const exchangeSetDetailsMockData: any = {
