a6fb8be41c398c73a80c20993881ae016ce25d68
Richard Hernandez <richard.hernandez@ukho.gov.uk>
2025-01-16 16:14:34 +0000

    Import CdkTable into Table.module restored test

--- src/app/shared/components/ukho-table/table.module.ts
+++ src/app/shared/components/ukho-table/table.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { TableComponent } from './table.component';
 import { CommonModule } from '@angular/common';
-import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTable, CdkTableModule } from '@angular/cdk/table';
 import { SortDirective } from './sort.directive';
 import { SortHeaderDirective } from './sort-header.directive';
 
@@ -60,6 +60,6 @@ const EXPORTED_DECLARATIONS = [
 @NgModule({
   declarations: [EXPORTED_DECLARATIONS],
   exports: [EXPORTED_DECLARATIONS],
-  imports: [CommonModule, CdkTableModule],
+  imports: [CommonModule, CdkTableModule,CdkTable],
 })
 export class TableModule { }
