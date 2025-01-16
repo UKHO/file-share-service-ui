12da840cdd1089b86b78565bc13ea15f1bec6faa
Richard Hernandez <richard.hernandez@ukho.gov.uk>
2025-01-16 13:40:24 +0000

    Undo last change

--- src/app/shared/components/ukho-table/table.module.ts
+++ src/app/shared/components/ukho-table/table.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { TableComponent } from './table.component';
 import { CommonModule } from '@angular/common';
-import { CDK_TABLE, CdkTableModule } from '@angular/cdk/table';
+import { CdkTableModule } from '@angular/cdk/table';
 import { SortDirective } from './sort.directive';
 import { SortHeaderDirective } from './sort-header.directive';
 
@@ -54,7 +54,7 @@ const EXPORTED_DECLARATIONS = [
   SortDirective,
   SortHeaderDirective,
 
-  CDK_TABLE
+  
 ];
 
 @NgModule({
