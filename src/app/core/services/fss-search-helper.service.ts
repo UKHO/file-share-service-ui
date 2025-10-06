88d3202c7878ffa4da5542c442f6499b4ea3bf62
richardAhz <Richard.Hernandez@ukho.gov.uk>
2025-10-06 11:37:59 +0100

    more console logs

--- src/app/core/services/fss-search-helper.service.ts
+++ src/app/core/services/fss-search-helper.service.ts
@@ -61,7 +61,10 @@ export class FssSearchHelperService {
   }
 
   getFieldValue(fieldText: string, fields: Field[]) {
+    console.log('Get Field Value'); //Rhz
+    console.log('Field Text', fieldText); //Rhz
     const selectedFieldValue: any = fields.find(f => f.text === fieldText)?.value!;
+    console.log('Selected Field Value', selectedFieldValue); //Rhz
     return selectedFieldValue;
   }
 
