0a11c713dee2a61290831ea0e1019670aad00eda
rushdynajath <rushdy.najath@ukho.gov.uk>
2026-07-08 10:36:33 +0100

    clean up

--- jest.config.js
+++ jest.config.js
@@ -7,9 +7,6 @@ module.exports = {
   roots: ['<rootDir>/Tests/UnitTests/'],
   testMatch: ['**/+(*.)+(spec).+(ts)'],
   setupFilesAfterEnv:  ['<rootDir>/setup-jest.ts'],
-  transformIgnorePatterns: [
-    'node_modules/(?!.*\\.mjs$|@ukho/admiralty-angular|@ukho/admiralty-core)'
-  ],
   collectCoverage: true,
   coverageReporters: ['html','cobertura'],
   coverageDirectory: 'coverage',
